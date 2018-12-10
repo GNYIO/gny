2e362f7834e9634420f3880708e52d1de7a89dfa
a1300 <matthias-knopp@gmx.net>
2018-12-10 22:52:09 +0100

    chore: remove unused props in core/chains

--- src/core/chains.ts
+++ src/core/chains.ts
@@ -2,14 +2,11 @@ import async = require('async');
 import * as fs from 'fs';
 import * as path from 'path';
 import * as _ from 'lodash';
-import Sandbox = require('asch-sandbox');
 import rmdir = require('rimraf');
 import Router = require('../utils/router');
 
 export class Chains {
   private library: any;
-  private modules: any;
-  private baseDir: string;
 
   constructor(scope: any) {
     this.library = scope;
