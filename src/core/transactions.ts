1d116d33ad8163d2fedb107a19db0ab10a3efba8
a1300 <matthias-knopp@gmx.net>
2018-12-15 19:06:59 +0100

    chore: moved class TransactionPool into own file

--- src/core/transactions.ts
+++ src/core/transactions.ts
@@ -4,53 +4,10 @@ import Router from '../utils/router';
 import LimitCache from '../utils/limit-cache';
 import addressHelper = require('../utils/address');
 import transactionMode from '../utils/transaction-mode';
+import TransactionPool from '../utils/transaction-pool';
 import { Modules, IScope } from '../interfaces';
 
-class TransactionPool {
-  private index: Map<any, any>;
-  private unConfirmed: any[];
-  constructor() {
-    this.index = new Map()
-    this.unConfirmed = []
-  }
 
-  public add(trs: any) {
-    this.unConfirmed.push(trs)
-    this.index.set(trs.id, this.unConfirmed.length - 1)
-  }
-
-  public remove(id: string) {
-    const pos = this.index.get(id)
-    delete this.index[id]
-    this.unConfirmed[pos] = null
-  }
-
-  public has(id: string) {
-    const pos = this.index.get(id)
-    return pos !== undefined && !!this.unConfirmed[pos]
-  }
-
-  public getUnconfirmed() {
-    const a: any[] = []
-
-    for (let i = 0; i < this.unConfirmed.length; i++) {
-      if (this.unConfirmed[i]) {
-        a.push(this.unConfirmed[i])
-      }
-    }
-    return a
-  }
-
-  public clear() {
-    this.index = new Map()
-    this.unConfirmed = []
-  }
-
-  public get(id: string) {
-    const pos = this.index.get(id)
-    return this.unConfirmed[pos]
-  }
-}
 
 // Constructor
 class Transactions {
@@ -67,7 +24,6 @@ class Transactions {
     this.failedTrsCache = new LimitCache();
 
     this.attachApi();
-    // setImmediate(cb, null, self)
   }
 
 
