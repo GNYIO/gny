13f759955967bdc676e79aefee84b421b11b82aa
a1300 <matthias-knopp@gmx.net>
2018-12-19 19:06:25 +0100

    chore: refactored blocksApi

--- packages/api/blocksApi.ts
+++ packages/api/blocksApi.ts
@@ -41,12 +41,9 @@ export default class BlocksApi  {
     router.get('/getSupply', this.getSupply);
     router.get('/getStatus', this.getStatus);
 
-    // v2
-    router.get('/:heightOrId', this.getBlockByHeightOrId);
-
     // Configuration
-    router.use((req, res) => {
-      res.status(500).send({ success: false, error: 'API endpoint not found' })
+    router.use((req: Request, res: Response) => {
+      res.status(500).send({ success: false, error: 'API endpoint not found' });
     })
 
     this.library.network.app.use('/api/blocks', router)
@@ -77,123 +74,96 @@ export default class BlocksApi  {
       },
     });
     if (!report) {
-      return next(this.library.scheme.getLastError())
+      return next(this.library.scheme.getLastError());
     }
 
     try {
-      let block
+      let block;
       if (query.id) {
-        block = await global.app.sdb.getBlockById(query.id)
+        block = await global.app.sdb.getBlockById(query.id);
       } else if (query.height !== undefined) {
-        block = await global.app.sdb.getBlockByHeight(query.height)
+        block = await global.app.sdb.getBlockByHeight(query.height);
       } else {
-        return next('you need to provide either id or height')
+        return next('you need to provide either id or height');
       }
 
       if (!block) {
-        return next('Block not found')
+        return next('Block not found');
       }
       return res.json({ block: block });
     } catch (e) {
-      this.library.logger.error(e)
-      return next('Server error')
+      this.library.logger.error(e);
+      return next('Server error');
     }
   }
 
 
-  private getBlocks = (req, cb) => async (req) => {
-    const query = req.query
-    const offset = query.offset ? Number(query.offset) : 0
-    const limit = query.limit ? Number(query.limit) : 20
-    let minHeight
-    let maxHeight
-    let needReverse = false
+  private getBlocks = async (req: Request, res: Response) => {
+    const query = req.query;
+    const offset = query.offset ? Number(query.offset) : 0;
+    const limit = query.limit ? Number(query.limit) : 20;
+    let minHeight;
+    let maxHeight;
+    let needReverse = false;
     if (query.orderBy === 'height:desc') {
-      needReverse = true
-      maxHeight = this.modules.blocks.getLastBlock().height - offset
-      minHeight = (maxHeight - limit) + 1
-      minHeight = minHeight > 0 ? minHeight : 0
+      needReverse = true;
+      maxHeight = this.modules.blocks.getLastBlock().height - offset;
+      minHeight = (maxHeight - limit) + 1;
+      minHeight = minHeight > 0 ? minHeight : 0;
     } else {
-      minHeight = offset
-      maxHeight = (offset + limit) - 1
+      minHeight = offset;
+      maxHeight = (offset + limit) - 1;
     }
-    const withTransactions = !!query.transactions
-    let blocks = await this.modules.blocks.getBlocks(minHeight, maxHeight, withTransactions)
+    const withTransactions = !!query.transactions;
+    let blocks = await this.modules.blocks.getBlocks(minHeight, maxHeight, withTransactions);
     if (needReverse) {
-      blocks = _.reverse(blocks)
+      blocks = _.reverse(blocks);
     }
-    const count = global.app.sdb.blocksCount
-    return { count, blocks }
+    const count = global.app.sdb.blocksCount;
+    return res.json({ count, blocks });
   }
 
-  private getHeight = (req, cb) => {
-    if (!this.loaded) {
-      return cb('Blockchain is loading')
-    }
-    return cb(null, { height: this.modules.blocks.getLastBlock().height })
+  private getHeight = (req: Request, res: Response) => {
+    // if (!this.loaded) {
+    //   return cb('Blockchain is loading')
+    // }
+    return res.json({ height: this.modules.blocks.getLastBlock().height });
   }
 
-  private getMilestone = (req, cb) => {
-    if (!this.loaded) {
-      return cb('Blockchain is loading')
-    }
-    const height = this.modules.blocks.getLastBlock().height
-    return cb(null, { milestone: this.blockreward.calculateMilestone(height) })
+  private getMilestone = (req: Request, res: Response) => {
+    // if (!this.loaded) {
+    //   return res('Blockchain is loading')
+    // }
+    const height = this.modules.blocks.getLastBlock().height;
+    return res.json({ milestone: this.blockreward.calculateMilestone(height) });
   }
 
-  private getReward = (req, cb) => {
-    if (!this.loaded) {
-      return cb('Blockchain is loading')
-    }
-    const height = this.modules.blocks.getLastBlock().height
-    return cb(null, { reward: this.blockreward.calculateReward(height) })
+  private getReward = (req: Request, res: Response, next) => {
+    // if (!this.loaded) {
+    //   return res('Blockchain is loading')
+    // }
+    const height = this.modules.blocks.getLastBlock().height;
+    return res.json({ reward: this.blockreward.calculateReward(height) });
   }
 
-  private getSupply = (req, cb) => {
-    if (!this.loaded) {
-      return cb('Blockchain is loading')
-    }
-    const height = this.modules.blocks.getLastBlock().height
-    return cb(null, {
+  private getSupply = (req: Request, res: Response, next) => {
+    const height = this.modules.blocks.getLastBlock().height;
+    return res.json({
       supply: this.blockreward.calculateSupply(height)
-    })
+    });
   }
 
-  private getStatus = (req, cb) => {
-    if (!this.loaded) {
-      return cb('Blockchain is loading')
-    }
-    const height = this.modules.blocks.getLastBlock().height
-    return cb(null, {
+  private getStatus = (req: Request, res: Response, next) => {
+    // if (!this.loaded) {
+    //   return res('Blockchain is loading')
+    // }
+    const height = this.modules.blocks.getLastBlock().height;
+    return res.json({
       height,
       fee: this.library.base.block.calculateFee(),
       milestone: this.blockreward.calculateMilestone(height),
       reward: this.blockreward.calculateReward(height),
       supply: this.blockreward.calculateSupply(height),
-    })
-  }
-
-  private getBlockByHeightOrId = async (req) => {
-    const idOrHeight = req.params.idOrHeight
-    let block
-    if (idOrHeight.length === 64) {
-      let id = idOrHeight
-      block = await global.app.sdb.getBlockById(id)
-    } else {
-      let height = Number(idOrHeight)
-      if (Number.isInteger(height) && height >= 0) {
-        block = await global.app.sdb.getBlockByHeight(height)
-      }
-    }
-    if (!block) throw new Error('Block not found')
-    if (!!req.query.transactions) {
-      const transactions = await global.app.sdb.findAll('Transaction', {
-        condition: {
-          height: block.height,
-        },
-      })
-      block.transactions = transactions
-    }
-    return { block }
+    });
   }
 }
