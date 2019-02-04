6a7f967cebc81d8a7d84302c059a2cef56f6edf2
a1300 <matthias-knopp@gmx.net>
2019-02-04 21:42:47 +0100

    fix: copyStaticAssets

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,15 +1,8 @@
 import * as shell from 'shelljs';
 import * as PeerId from 'peer-id';
 import * as fs from 'fs';
-import * as path from 'path';
 
-const pathToP2PKey = path.join(__dirname, 'dist', 'p2pKey.json');
-let content;
-if (fs.existsSync(pathToP2PKey)) {
-  content = fs.readFileSync(pathToP2PKey, { encoding: 'utf8' });
-}
-
-shell.exec('rm -rf dist/data/');
+shell.exec('rm -rf dist/data');
 shell.cp('-r', 'proto', 'dist');
 shell.cp('genesisBlock.json', 'dist');
 shell.cp('config.json', 'dist');
@@ -17,12 +10,10 @@ shell.cp('config.json', 'dist');
 shell.mkdir('-p', 'dist/data/blocks');
 shell.mkdir('-p', 'dist/logs');
 
-if (!content) {
-  PeerId.create({}, (err, id) => {
+PeerId.create({}, (err, id) => {
+  if (err) { throw err; }
+  const jsonId = JSON.stringify(id.toJSON(), null, 2);
+  fs.writeFile('./dist/p2p_key.json', jsonId, (err) => {
     if (err) { throw err; }
-    const jsonId = JSON.stringify(id.toJSON(), null, 2);
-    fs.writeFile('./dist/p2pKey.json', jsonId, (err) => {
-      if (err) { throw err; }
-    });
   });
-}
+});
