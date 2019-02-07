660a045b65db3c82d70de939891eed2a36050051
a1300 <matthias-knopp@gmx.net>
2019-02-07 18:54:15 +0100

    chore: create x peer nodes

--- createSecondNode.js
+++ createSecondNode.js
@@ -1,61 +1,94 @@
 const shell = require('shelljs');
 const PeerId = require('peer-id');
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
+const crypto = require('crypto');
 
 
 if (!fs.existsSync('./dist')) {
-  throw new Error('first run: "npm run tsc"');
+  console.error('first run: "npm run tsc"');
+  process.exit(1);
 }
 
-shell.exec('rm -rf dist2');
 
-shell.exec('cp -r ./dist ./dist2');
+const HOW_MANY_NODES = Number(process.argv[2]) || 2;
+if (Number.isNaN(HOW_MANY_NODES) || HOW_MANY_NODES > 101) {
+  console.error('please provide a number smaller then or equal to 101');
+  process.exit(1);
+}
+if (HOW_MANY_NODES <= 1) {
+  console.log('number must be greater then 1');
+  process.exit(1);
+}
 
-let firstConfig = fs.readFileSync('./dist/config.json', { encoding: 'utf8' });
+
+// fillter all dist directories except "dist"
+const distDirectories = fs.readdirSync(__dirname)
+  .filter((item) => RegExp('dist[1-9]+').test(item));
+
+distDirectories.forEach((dir) => {
+  shell.exec(`rm -rf ${dir}`);
+});
+
+// copy dist directories
+for (let i = 1; i < HOW_MANY_NODES; ++i) {
+  shell.exec(`cp -r ./dist ./dist${i}`);
+}
+
+// read dist/ data
+let firstConfig = fs.readFileSync('./config.json', { encoding: 'utf8' });
 firstConfig = JSON.parse(firstConfig);
-
 let firstKey = fs.readFileSync('./dist/p2p_key.json', { encoding: 'utf8' });
 firstKey = JSON.parse(firstKey);
 
 
-// create secondKey and save it immediately
-PeerId.create({}, (err, secondKey) => {
-  secondKey = secondKey.toJSON();
 
-  shell.exec('rm ./dist2/p2p_key.json');
-  fs.writeFileSync('./dist2/p2p_key.json', JSON.stringify(secondKey, null, 2), { encoding: 'utf8' });
+for (let i = 1; i < HOW_MANY_NODES; ++i) {
+  const id = new PeerId(crypto.randomBytes(32));
 
-
-  const secondConfig = _.cloneDeep(firstConfig);
-
-  // change
-  secondConfig.port = firstConfig.port + 2;
-  secondConfig.peerPort = firstConfig.peerPort + 2;
+  shell.exec(`rm ./dist${i}/p2p_key.json`);
+  fs.writeFileSync(`./dist${i}/p2p_key.json`, JSON.stringify(id, null, 2), { encoding: 'utf8' });
+}
 
 
-  const wayToCallFirstAddress = `/ip4/${firstConfig.address}/tcp/${firstConfig.peerPort}/ipfs/${firstKey.id}`;
-  const wayToCallSecondAddress = `/ip4/${secondConfig.address}/tcp/${secondConfig.peerPort}/ipfs/${secondKey.id}`;
+
+const MULTIADDRS_FIRST = `/ip4/${firstConfig.address}/tcp/${firstConfig.peerPort}/ipfs/${firstKey.id}`;
+
+const INTIAL_SECRETS = firstConfig.forging.secret;
+const count = INTIAL_SECRETS.length;
+const onePart = Math.floor(count / HOW_MANY_NODES);
+
+for (let i = 0; i < HOW_MANY_NODES; ++i) {
+
+  const nthConfig = _.cloneDeep(firstConfig);
+  nthConfig.port += (i * 2);
+  nthConfig.peerPort += (i * 2);
+
+  const from = i * onePart;
+  let to = (i + 1) * onePart;
+
+  // mind the rest of the division (affects only last node)
+  if ((i + 1) === HOW_MANY_NODES) {
+    to = undefined;
+  }
+
+  const partitionedSecrets = INTIAL_SECRETS.slice(from, to);
+  nthConfig.forging.secret = partitionedSecrets;
 
 
-  const DIVIDE_BY = 2;
-  const count = firstConfig.forging.secret.length;
-  const onePart = Math.floor(count / DIVIDE_BY);
-  const rest = count % DIVIDE_BY;
+  if (i === 0) {
+    nthConfig.bootstrap = null;
 
-  const firstSecrets = firstConfig.forging.secret.slice(0, onePart);
-  const secondSecrets = firstConfig.forging.secret.slice(onePart);
+    shell.exec('rm ./dist/config.json');
+    fs.writeFileSync('./dist/config.json', JSON.stringify(nthConfig, null, 2), { encoding: 'utf8' });
+  } else {
+    nthConfig.bootstrap = MULTIADDRS_FIRST;
 
-
-  firstConfig.peers.bootstrap = wayToCallSecondAddress;
-  secondConfig.peers.bootstrap = wayToCallFirstAddress;
-
-  firstConfig.forging.secret = firstSecrets;
-  secondConfig.forging.secret = secondSecrets;
+    shell.exec(`rm ./dist${i}/config.json`);
+    fs.writeFileSync(`./dist${i}/config.json`, JSON.stringify(nthConfig, null, 2), { encoding: 'utf8' });
+  }
+}
 
 
-  fs.writeFileSync('./dist/config.json', JSON.stringify(firstConfig, null, 2), { encoding: 'utf8' });
-  fs.writeFileSync('./dist2/config.json', JSON.stringify(secondConfig, null, 2), { encoding: 'utf8' });
-
-
-});
+process.exit(1);
