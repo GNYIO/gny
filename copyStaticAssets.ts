f96c83d02a40e032c7826f203ba0363527fd865a
a1300 <matthias-knopp@gmx.net>
2019-02-17 16:44:24 +0100

    chore: add typescript types from polkadot for p2p packages

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,5 +1,5 @@
 import * as shell from 'shelljs';
-import * as PeerId from 'peer-id';
+import * as PeerInfo from 'peer-info';
 import * as fs from 'fs';
 
 shell.exec('rm -rf dist/data');
@@ -11,9 +11,8 @@ shell.cp('gnyd', 'dist');
 shell.mkdir('-p', 'dist/data/blocks');
 shell.mkdir('-p', 'dist/logs');
 
-PeerId.create({}, (err, id) => {
-  if (err) { throw err; }
-  const jsonId = JSON.stringify(id.toJSON(), null, 2);
+PeerInfo.create((err, peerInfo) => {
+  const jsonId = JSON.stringify(peerInfo.id.toJSON());
   fs.writeFile('./dist/p2p_key.json', jsonId, (err) => {
     if (err) { throw err; }
   });
