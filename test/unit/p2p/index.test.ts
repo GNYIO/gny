52748ba996acfac4cd153cfde2768a024a148b50
a1300 <matthias-knopp@gmx.net>
2019-02-23 09:50:24 +0100

    chore: add peer2peer tests

--- test/unit/p2p/index.test.ts
+++ test/unit/p2p/index.test.ts
@@ -1,9 +1,10 @@
 import { Peer2Peer } from '../../../packages/p2p/index';
 import { createPeerInfo } from '../../../packages/p2p/createPeerInfo';
 import { ILogger, P2PMessage } from '../../../src/interfaces';
-
+import * as PeerInfo from 'peer-info';
 
 const delay = (x: number): Promise<void> => new Promise((resolve) => setInterval(resolve, x));
+const getMultiAddr = (peerInfo: PeerInfo): string => peerInfo.multiaddrs.toArray()[0].toString();
 
 async function createNewPeer2PeerNode (port: number = 0, bootstrapNode?: string, bootstrapInterval?: number) {
   const logger: ILogger = {
@@ -47,14 +48,7 @@ describe('p2p', () => {
       done();
     });
 
-    it('after stopping node with stopAsync() it should be stopped', async (done) => {
-      expect.assertions(1);
-      const node1 = await createNewPeer2PeerNode();
-      await node1.startAsync();
-      await node1.stopAsync();
-      expect(node1.isStarted()).toBeFalsy();
-      done();
-    });
+
 
     // it.skip('starting two p2p-nodes on the same port results in a EADDRINUSE in use error', async (done) => {
     //   const node1 = await createNewPeer2PeerNode(6000);
@@ -65,31 +59,58 @@ describe('p2p', () => {
     // }, 2000);
   });
 
-  describe('shutodwn', () => {
-    it.skip('shutdown node should have no peers in peerBook', async (done) => {
+  describe('stoppage', () => {
+    it('stopped node should have no peers in peerBook', async (done) => {
+      expect.assertions(2);
+      const BOOTSTRAP_INTERVAL = 500; // ms
+
+      const node1 = await createNewPeer2PeerNode();
+      await node1.startAsync();
+
+      const bootstrap_address_for_node2 = getMultiAddr(node1.peerInfo);
+      const node2 = await createNewPeer2PeerNode(5011, bootstrap_address_for_node2, BOOTSTRAP_INTERVAL);
+      await node2.startAsync();
+
+      await delay(1000);
+      expect(node2.peerBook.getAllArray().length).toEqual(1);
+
+      await node2.stopAsync();
+      expect(node2.peerBook.getAllArray().length).toEqual(0);
+
+      // cleanup
+      await node1.stopAsync();
+      done();
+    }, 5000);
+
+    it('after stopping node with stopAsync() it should be stopped', async (done) => {
+      expect.assertions(1);
+      const node1 = await createNewPeer2PeerNode();
+      await node1.startAsync();
+      await node1.stopAsync();
+      expect(node1.isStarted()).toBeFalsy();
       done();
     });
   });
 
   describe('bootstrap', () => {
     it('node2 should dial to node1 on bootstrap', async (done) => {
+      expect.assertions(2);
+
       const node1 = await createNewPeer2PeerNode(5010);
       await node1.startAsync();
 
       const BOOTSTRAP_INTERVAL = 2000; // ms
-      const bootstrap_address_for_node2 = node1.peerInfo.multiaddrs.toArray()[0].toString();
+      const bootstrap_address_for_node2 = getMultiAddr(node1.peerInfo);
       const node2 = await createNewPeer2PeerNode(5011, bootstrap_address_for_node2, BOOTSTRAP_INTERVAL);
       await node2.startAsync();
 
       // wait for periodically bootstrap method to run at least one time
       await delay(5000);
 
-      const info2 = node2.peerInfo;
-      const node1_know_about_node2 = node1.peerBook.has(info2);
-      expect(node1_know_about_node2).toBeTruthy();
+      const node1_knows_about_node2 = node1.peerBook.has(node2.peerInfo);
+      expect(node1_knows_about_node2).toBeTruthy();
 
-      const info1 = node1.peerInfo;
-      const node2_knows_about_node1 = node2.peerBook.has(info1);
+      const node2_knows_about_node1 = node2.peerBook.has(node1.peerInfo);
       expect(node2_knows_about_node1).toBeTruthy();
 
       // cleanup
@@ -106,19 +127,20 @@ describe('p2p', () => {
 
       const BOOTSTRAP_INTERVAL = 2000; // ms
 
-      const node1_address = node1.peerInfo.multiaddrs.toArray()[0].toString();
-      const node2 = await createNewPeer2PeerNode(6002, node1_address, BOOTSTRAP_INTERVAL);
+      const node1_addr = getMultiAddr(node1.peerInfo);
+      const node2 = await createNewPeer2PeerNode(6002, node1_addr, BOOTSTRAP_INTERVAL);
       await node2.startAsync();
 
       // give the node2 time to connect to node1
       await delay(3000);
       expect(node2.peerBook.getAllArray().length).toEqual(1);
 
-      // stop node1 and restart
+      // stop node1
       await node1.stopAsync();
       await delay(1000);
       expect(node2.peerBook.getAllArray().length).toEqual(0);
 
+      // restart node1
       await delay(1000);
       await node1.startAsync();
 
@@ -138,21 +160,27 @@ describe('p2p', () => {
     it.skip('stop node that had 1 peer in peerBook has no peers on restart in peerBook', async (done) => {
       done();
     });
+    it.skip('multiaddr-string passed as bootstrap node connect nodes', async (done) => {});
+
+    it.skip('connect periodically to "bootstrap" node', async (done) => {
+      done();
+    });
   });
 
   describe('broadcast', () => {
     it('node 1,2,3: node 1 -> 2; and 2 -> 3 are connected. If I broadcast a message from node 1; node 3 should get the message', async (done) => {
       expect.assertions(3);
+
       const BOOTSTRAP_INTERVAL = 2000;
 
       const node1 = await createNewPeer2PeerNode(5000, undefined, BOOTSTRAP_INTERVAL);
       await node1.startAsync();
 
-      const bootstrap_for_node2 = node1.peerInfo.multiaddrs.toArray()[0].toString();
+      const bootstrap_for_node2 = getMultiAddr(node1.peerInfo);
       const node2 = await createNewPeer2PeerNode(5001, bootstrap_for_node2, BOOTSTRAP_INTERVAL);
       await node2.startAsync();
 
-      const bootstrap_for_node3 = node2.peerInfo.multiaddrs.toArray()[0].toString();
+      const bootstrap_for_node3 = getMultiAddr(node2.peerInfo);
       const node3 = await createNewPeer2PeerNode(5002, bootstrap_for_node3, BOOTSTRAP_INTERVAL);
       await node3.startAsync();
 
@@ -182,8 +210,33 @@ describe('p2p', () => {
 
       done();
     }, 20000);
+
+    it('a broadcast should not notify own node if broadcasted from own server', async (done) => {
+      expect.assertions(0);
+
+      const node1 = await createNewPeer2PeerNode();
+      await node1.startAsync();
+
+      node1.subscribe('shouldNotBeCalled', (message: P2PMessage) => {
+        throw new Error('not allowed');
+      });
+
+      // broadcast message
+      await node1.broadcastAsync('shouldNotBeCalled', Buffer.from('hello'));
+
+      await delay(1000);
+
+      // cleanup
+      await node1.stopAsync();
+      done();
+    }, 2000);
   });
 
+  describe('dht', () => {
+    it.skip('discover peer via periodically DHT random walk', async (done) => {
+      done();
+    });
+  });
   // node creation
     // should start node async
     // should start node with callback
@@ -192,13 +245,7 @@ describe('p2p', () => {
     // should stop if on tcp-port is already in use from an http-server
     // should stop if tcp-port is already in use from other p2p-node
   // broadcast
-    // node 1, 2, 3; nodes 1 -> 2; and 2 -> 3 are connected. If I broadcast a message from node 1; node 3 should get the message with the "from" field from node 1
-    // like the test before, only that node 2 and 3 should have the node 1 in their peerBook
     // broadcasts shouldn't get "notified" if broadcasted from own server
-    // (normally) we only receive the "from" (simple string) from a broadcasted message; test that the message.peerInfo is correctly applied to the message
     // node that broadcasted message should be in the peerInfo
     // same broadcasted message should be "notified" just once
-  // bootstrap
-    // connect periodically to "bootstrap" node
-    // reconnect periodically to the "bootstrap" node, even if the bootstrap node is down for a few seconds
 });
