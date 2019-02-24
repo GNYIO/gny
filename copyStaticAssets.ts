61e5050a2df9955fa921d520f78ef3d081676941
a1300 <matthias-knopp@gmx.net>
2019-02-24 22:47:01 +0100

    chore: edit README to include docker-include and docker

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,7 @@
 import * as shell from 'shelljs';
 import * as PeerId from 'peer-id';
 import * as fs from 'fs';
+import * as path from 'path';
 
 shell.exec('rm -rf dist/data');
 shell.cp('-r', 'proto', 'dist');
@@ -19,4 +20,17 @@ PeerId.create({}, (err, id) => {
   fs.writeFile('./dist/p2p_key.json', jsonId, (err) => {
     if (err) { throw err; }
   });
+});
+
+fs.readFile(path.join(__dirname, 'ormconfig.json'), { encoding: 'utf8' }, (err, data) => {
+  if (err) { throw err; }
+  else {
+    const parsedData: any = JSON.parse(data);
+    const postgresPort = parsedData.port;
+    const redisPort = parsedData.cache.options.port;
+    const envContent = `POSTGRES_PORT=${postgresPort}\nREDIS_PORT=${redisPort}`;
+    fs.writeFile('dist/.env', envContent, { encoding: 'utf8' }, (err) => {
+      if (err) { throw err; }
+    });
+  }
 });
