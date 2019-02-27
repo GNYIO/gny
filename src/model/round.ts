96b965f2626e0c02a33de7cb6055ed528e66ec79
a1300 <matthias-knopp@gmx.net>
2019-02-27 17:30:41 +0100

    chore: remove unused model/ directory

--- src/model/round.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  table: 'rounds',
-  tableFields: [
-    { name: 'round', type: 'BigInt', primary_key: true },
-    { name: 'fee', type: 'BigInt', not_null: true },
-    { name: 'reward', type: 'BigInt', not_null: true },
-  ]
-};
