b7d15986a558fdc3664492999d869a14f7225440
a1300 <matthias-knopp@gmx.net>
2019-03-20 23:44:48 +0100

    chore: refactor class JsonSQLBuilder

--- packages/database-postgres/src/jsonSQLBuilder.ts
+++ packages/database-postgres/src/jsonSQLBuilder.ts
@@ -1,11 +1,11 @@
-import { FieldTypes } from './fieldTypes';
-import { deepCopy } from './codeContract';
-import { isString, isArray, isNumber } from 'util';
+import { isArray, isNumber } from 'util';
+
 const jsonSQL = require('json-sql')({
   separatedValues : false
 });
 import * as lodash from 'lodash';
 import { ModelSchema } from './modelSchema';
+import { ObjectLiteral } from 'typeorm';
 
 export const MULTI_SQL_SEPARATOR = ';';
 export enum SqlType {
@@ -17,119 +17,78 @@ export enum SqlType {
   Other = 9,
 }
 
+
+export type SqlParameters = Array<any> | ObjectLiteral;
+
+export type SqlAndParameters = {
+  type: SqlType;
+  query: string;
+  parameters?: SqlParameters;
+  expectEffected?: boolean;
+};
+
+
 export class JsonSqlBuilder {
 
-  getTableName(key) {
-    return lodash.snakeCase(key) + 's';
+  private getTableName(key: string) {
+    return lodash.lowerCase();
   }
 
-  getPrimaryKeyCondition(diagramModel, columnName) {
-    return diagramModel.setPrimaryKey({}, columnName);
+  private getPrimaryKeyCondition(schema: ModelSchema, columnName) {
+    return schema.setPrimaryKey({}, columnName);
   }
 
-  buildDropSchema(schema: ModelSchema) {
-    return 'drop table "' + this.getTableName(schema.modelName) + '"';
-  }
-
-  buildSchema(schema: ModelSchema) {
-    const t = new Array;
-    const obj = Object.assign({
-      type : 'create'
-    }, deepCopy(schema.schemaObject));
-    schema.jsonProperties.forEach(function(name) {
-      return obj.tableFields.find(function(functionImport) {
-        return functionImport.name === name;
-      }).type = FieldTypes.Text;
-    });
-    obj.tableFields.filter(function(object) {
-      return isString(object.unique);
-    }).forEach(function(obj) {
-      return Reflect.deleteProperty(obj, 'unique');
-    });
-    const a = jsonSQL.build(obj);
-    t.push(a.query);
-    const tableName = this.getTableName(schema.modelName);
-    schema.indexes.forEach(function(f) {
-      t.push(jsonSQL.build({
-        type : 'index',
-        table : tableName,
-        name : tableName + '_' + f.name,
-        indexOn : f.properties.join(',')
-      }).query);
-    });
-    const transferArr = obj.tableFields.filter(function(studiesList) {
-      return true === studiesList.unique;
-    }).map(function(engineDiscovery) {
-      return engineDiscovery.name;
-    });
-    const _eventQueue = schema.uniqueIndexes.filter(function(oldnode) {
-      return !(1 === oldnode.properties.length && transferArr.some(function(canCreateDiscussions) {
-        return canCreateDiscussions === oldnode.properties[0];
-      }));
-    });
-    schema.isCompsiteKey && _eventQueue.push({
-      name : 'composite_primary_key',
-      properties : schema.compositeKeys
-    });
-    _eventQueue.forEach(function(f) {
-      t.push(jsonSQL.build({
-        type : 'unique',
-        table : tableName,
-        name : tableName + '_' + f.name,
-        uniqueOn : f.properties.join(',')
-      }).query);
-    });
-    return t;
-  }
-
-  buildInsert(args, key) {
-    var i = {
+  public buildInsert(schema: ModelSchema, fieldValues: ObjectLiteral) {
+    const i = {
       type : SqlType.Insert
     };
-    return Object.assign(i, jsonSQL.build({
+    const result: SqlAndParameters = Object.assign(i, jsonSQL.build({
       type : 'insert',
-      table : this.getTableName(args.modelName),
-      values : this.replaceJsonFields(args, key)
+      table : this.getTableName(schema.modelName),
+      values : this.replaceJsonFields(schema, fieldValues)
     }));
+    return result;
   }
 
-  buildDelete(args, params) {
-    var i = {
+  public buildDelete(schema: ModelSchema, primarykey: ObjectLiteral) {
+    const i = {
       type : SqlType.Delete
     };
-    return Object.assign(i, jsonSQL.build({
+    const result: SqlAndParameters = Object.assign(i, jsonSQL.build({
       type : 'remove',
-      table : this.getTableName(args.modelName),
-      condition : this.getPrimaryKeyCondition(args, params)
+      table : this.getTableName(schema.modelName),
+      condition : this.getPrimaryKeyCondition(schema, primarykey)
     }));
+    return result;
   }
 
-  buildUpdate(args, environment, type, selector) {
-    var tableName = this.getTableName(args.modelName);
-    var func = this.getPrimaryKeyCondition(args, environment);
-    func._version_ = selector;
-    var a = {
+  public buildUpdate(schema: ModelSchema, primaryKey: ObjectLiteral, fieldValues: ObjectLiteral, version: number) {
+    const tableName = this.getTableName(schema.modelName);
+    const obj = this.getPrimaryKeyCondition(schema, primaryKey);
+    obj._version_ = version;
+    const a = {
       type : SqlType.Update
     };
-    return Object.assign(a, jsonSQL.build({
+    const result: SqlAndParameters = Object.assign(a, jsonSQL.build({
       type : 'update',
       table : tableName,
-      modifier : this.replaceJsonFields(args, type),
-      condition : func
+      modifier : this.replaceJsonFields(schema, fieldValues),
+      condition : obj
     }));
+    return result;
   }
 
-  buildSelect(args, index, condition, size, object, fn) {
-    var tableName = this.getTableName(args.modelName);
-    var options = undefined;
-    if (isArray(index)) {
-      var result = index || args.properties.map(function(canCreateDiscussions) {
-        return args.schemaObject.table + '.' + canCreateDiscussions;
+  public buildSelect(schema: ModelSchema, fields: string[] | ObjectLiteral, where?: ObjectLiteral, resultRange?: any, sort?: any, join?: any) {
+    const tableName = this.getTableName(schema.modelName);
+    let options = undefined;
+    if (isArray(fields)) {
+      const result = fields || schema.properties.map(function(canCreateDiscussions) {
+        return schema.schemaObject.table + '.' + canCreateDiscussions;
       });
-      var query = isNumber(size) ? {
-        limit : size
-      } : size || {};
-      var obj = object || {};
+      const query = isNumber(resultRange) ? {
+        limit : resultRange
+      } : resultRange || {};
+      const obj = sort || {};
       /** @type {boolean} */
       var _iteratorNormalCompletion3 = true;
       /** @type {boolean} */
@@ -162,35 +121,32 @@ export class JsonSqlBuilder {
         type : 'select',
         table : tableName,
         fields : result,
-        condition : condition,
+        condition : where,
         limit : query.limit,
         offset : query.offset,
         sort : obj,
-        join : fn
+        join : join
       };
     } else {
-      /** @type {!Function} */
-      var show = index;
-      /** @type {!Object} */
+      const show = fields;
       options = Object.assign({
         type : 'select',
         table : tableName
       }, show);
     }
-    var static_events = {
+    const static_events = {
       type : SqlType.Select
     };
     return Object.assign(static_events, jsonSQL.build(options));
   }
 
-  replaceJsonFields(schema: ModelSchema, options) {
+  private replaceJsonFields(schema: ModelSchema, options) {
     if (0 === schema.jsonProperties.length) {
       return options;
     }
     const extractedTargets = Object.assign({}, options);
     schema.jsonProperties.forEach(function(target) {
       if (Reflect.has(options, target)) {
-        /** @type {string} */
         extractedTargets[target] = JSON.stringify(options[target]);
       }
     });
