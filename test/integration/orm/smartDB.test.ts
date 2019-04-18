a1b2f71a90c3408015acacac2330d090dd50f36f
a1300 <matthias-knopp@gmx.net>
2019-04-18 02:26:21 +0200

    test: more SmartDB integration tests, #89

--- test/integration/orm/smartDB.test.ts
+++ test/integration/orm/smartDB.test.ts
@@ -1735,4 +1735,74 @@ describe('integration - SmartDB', () => {
   it.skip('multiple modifications in one block should incrase the _version_ of the entity', async done => {
     done();
   });
+
+  it('exists() - entity exists in DB after beginBlock()', async done => {
+    await saveGenesisBlock(sut);
+
+    const block = createBlock(1);
+    const key = {
+      id: block.id,
+    };
+    sut.beginBlock(block);
+    await sut.commitBlock();
+
+    const exists = await sut.exists('Block', key);
+    expect(exists).toEqual(true);
+    done();
+  });
+
+  it('exists() - entity does not exists in DB', async done => {
+    await saveGenesisBlock(sut);
+
+    const block = createBlock(1);
+    sut.beginBlock(block);
+
+    const key = {
+      id: 'notValidId',
+    };
+    const exists = await sut.exists('Block', key);
+    expect(exists).toEqual(false);
+    done();
+  });
+
+  it('exists() - can access item after createOrLoad() (false)', async done => {
+    await saveGenesisBlock(sut);
+
+    // create() or createOrLoad() does not save entity directly to DB
+    const account = await sut.createOrLoad('Variable', {
+      key: 'key',
+      value: 'value',
+    });
+
+    const exists = await sut.exists('Variable', { key: 'key' });
+    expect(exists).toEqual(false);
+    done();
+  });
+
+  it.skip('should exists() always hit the database?', async done => {
+    done();
+  });
+
+  it.skip('should createOrLoad("Variable") be cached and returned with "sdb.get()"', async done => {
+    done();
+  });
+
+  it.skip('why is the bookkeeper variable not found?', async done => {
+    done();
+  });
+
+  it('createOrLoad() - after createOrLoad entity should be cached', async done => {
+    const variable = await sut.createOrLoad('Variable', {
+      key: 'key',
+      value: 'value',
+    });
+
+    const result = await sut.get('Variable', { key: 'key' });
+    expect(result).toEqual({
+      _version_: 1,
+      key: 'key',
+      value: 'value',
+    });
+    done();
+  });
 });
