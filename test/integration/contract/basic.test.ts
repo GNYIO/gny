7f4ae5dc6cdec5f1c17c36fd349d8b16eaf9bfc8
xpgeng <xpgeng.me@gmail.com>
2019-04-18 22:33:57 +0800

    chore: add a test for `transfer()` in /src/contract/basic.ts

--- test/integration/contract/basic.test.ts
+++ test/integration/contract/basic.test.ts
@@ -1,21 +1,70 @@
-import * as Basic from '../../../src/contract/basic';
+import basic from '../../../src/contract/basic';
+import BigNumber from 'bignumber.js';
+import { ILogger } from '../../../src/interfaces';
+import { SmartDB } from '../../../packages/database-postgres/src/smartDB';
+
+jest.mock('../../../packages/database-postgres/src/smartDB');
 
 describe('Consensus', () => {
-  let basic;
-
   beforeEach(done => {
-    basic = Basic.default;
+    const logger: ILogger = {
+      log: x => x,
+      trace: x => x,
+      debug: x => x,
+      info: x => x,
+      warn: x => x,
+      error: x => x,
+      fatal: x => x,
+    };
+
+    global.app = {
+      validate: jest.fn((type, value) => null),
+      util: {
+        address: {
+          isAddress: jest.fn(addr => true),
+          generateAddress: jest.fn(),
+        },
+        bignumber: BigNumber,
+      },
+      sdb: new SmartDB(logger),
+    };
+    done();
+  });
+
+  afterEach(done => {
     done();
   });
 
   describe('deleteCreatedVotes', () => {
+    let amount;
+    let recipient;
     beforeEach(done => {
+      amount = 100000;
+      recipient = 'G4GDW6G78sgQdSdVAQUXdm5xPS13t';
       done();
     });
 
-    it('should delete created votes', done => {
-      const validatedVotes = basic.transfer(1, '');
-      expect(validatedVotes);
+    it('should delete created votes', async done => {
+      const recipientAccount = {
+        address: recipient,
+        gny: amount,
+        username: null,
+      };
+      (basic as any).sender = {
+        address: 'Gsdfsdffs',
+        gny: 100000000,
+      };
+      (basic as any).block = {
+        height: 1,
+      };
+      (basic as any).trs = {
+        id: '180a6e8e69f56892eb212edbf0311c13d5219f6258de871e60fac54829979540',
+        timestamp: 12165155,
+      };
+      global.app.sdb.increase.mockReturnValue(recipientAccount);
+      global.app.sdb.load.mockReturnValue(recipientAccount);
+      const validatedVotes = await basic.transfer(amount, recipient);
+      expect(validatedVotes).toBeNull();
       done();
     });
   });
