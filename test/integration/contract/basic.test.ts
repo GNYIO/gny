939d1416a946242a6e676594e71224c64a95525e
xpgeng <xpgeng.me@gmail.com>
2019-04-19 23:08:58 +0800

    chore: add tests for /src/contract/basic.ts

--- test/integration/contract/basic.test.ts
+++ test/integration/contract/basic.test.ts
@@ -35,7 +35,7 @@ describe('Consensus', () => {
     done();
   });
 
-  describe('deleteCreatedVotes', () => {
+  describe('transfer', () => {
     let amount;
     let recipient;
     beforeEach(done => {
@@ -44,14 +44,14 @@ describe('Consensus', () => {
       done();
     });
 
-    it('should delete created votes', async done => {
+    it('should transfer to a recipient account', async done => {
       const recipientAccount = {
         address: recipient,
         gny: amount,
         username: null,
       };
       (basic as any).sender = {
-        address: 'Gsdfsdffs',
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
         gny: 100000000,
       };
       (basic as any).block = {
@@ -63,8 +63,227 @@ describe('Consensus', () => {
       };
       global.app.sdb.increase.mockReturnValue(recipientAccount);
       global.app.sdb.load.mockReturnValue(recipientAccount);
-      const validatedVotes = await basic.transfer(amount, recipient);
-      expect(validatedVotes).toBeNull();
+      const transfered = await basic.transfer(amount, recipient);
+      expect(transfered).toBeNull();
+      done();
+    });
+  });
+
+  describe('setUserName', () => {
+    let username;
+    let account;
+
+    beforeEach(done => {
+      username = 'xpgeng';
+      account = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        username: username,
+      };
+
+      done();
+    });
+
+    it('should set the user name with returning null', async done => {
+      // Assume the sender's usename is null
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        username: null,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      global.app.sdb.load.mockReturnValue(null);
+
+      const set = await basic.setUserName(username);
+      expect((basic as any).sender.username).toBe(username);
+      expect(set).toBeNull();
+      done();
+    });
+
+    it('should return Name already registered', async done => {
+      // Assume the sender's usename is null
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        username: null,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      global.app.sdb.load.mockReturnValue(account);
+
+      const set = await basic.setUserName(username);
+      expect(set).toBe('Name already registered');
+      done();
+    });
+
+    it('should return Name already set', async done => {
+      // Assume the sender's usename is null
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        username: 'xpgeng',
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      global.app.sdb.load.mockReturnValue(null);
+
+      const set = await basic.setUserName(username);
+      expect(set).toBe('Name already set');
+      done();
+    });
+  });
+
+  describe('setSecondPassphrase', () => {
+    let publicKey;
+    beforeEach(done => {
+      publicKey =
+        '2e65eb2d727adb6b39557c27093562aa93a9f8bad33a2d261acf4fce380c59b9';
+      done();
+    });
+
+    it('should set the second passphrase', async done => {
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        secondPublicKey: null,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      const set = await basic.setSecondPassphrase(publicKey);
+      expect((basic as any).sender.secondPublicKey).toBe(publicKey);
+      expect(set).toBeNull();
+      done();
+    });
+
+    it('should return Invalid account type', async done => {
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        secondPublicKey: null,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      global.app.util.address.isAddress.mockReturnValue(false);
+
+      const set = await basic.setSecondPassphrase(publicKey);
+      expect(set).toBe('Invalid account type');
+      done();
+    });
+
+    it('should return Password already set', async done => {
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000000,
+        secondPublicKey: publicKey,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+
+      const set = await basic.setSecondPassphrase(publicKey);
+      expect(set).toBe('Password already set');
+      done();
+    });
+  });
+
+  describe('lock', () => {
+    let height;
+    let amount;
+
+    beforeEach(done => {
+      done();
+    });
+
+    it('should lock the account by height and amout', async done => {
+      height = 5760 * 30 + 2;
+      amount = 99;
+      const vote = { delegate: 'liangpeili' };
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000100,
+        isLocked: 0,
+        lockHeight: 0,
+        lockAmount: 0,
+      };
+      (basic as any).block = {
+        height: 1,
+      };
+
+      global.app.sdb.lock.mockReturnValue(null);
+      global.app.sdb.update.mockReturnValue(null);
+      global.app.sdb.findAll.mockReturnValue([vote]);
+      global.app.sdb.increase.mockReturnValue(null);
+
+      const locked = await basic.lock(height, amount);
+      expect(locked).toBeNull();
+      done();
+    });
+
+    it('should return Height should be positive integer', async done => {
+      height = 1.2;
+      amount = 99;
+      const locked = await basic.lock(height, amount);
+      expect(locked).toBe('Height should be positive integer');
+      done();
+    });
+
+    it('should return Insufficient balance', async done => {
+      height = 5760 * 30 + 2;
+      amount = 101;
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000100,
+        isLocked: 0,
+        lockHeight: 0,
+        lockAmount: 0,
+      };
+
+      const locked = await basic.lock(height, amount);
+      expect(locked).toBe('Insufficient balance');
+      done();
+    });
+
+    it('should return Invalid lock height', async done => {
+      height = 2;
+      amount = 99;
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000100,
+        isLocked: 1,
+        lockHeight: 0,
+        lockAmount: 0,
+      };
+      (basic as any).block = {
+        height: 1,
+      };
+
+      const locked = await basic.lock(height, amount);
+      expect(locked).toBe('Invalid lock height');
+      done();
+    });
+
+    it('should return Invalid height or amount', async done => {
+      height = 0;
+      amount = 0;
+      (basic as any).sender = {
+        address: 'GBR31pwhxvsgtrQDfzRxjfoPB62r',
+        gny: 100000100,
+        isLocked: 1,
+        lockHeight: 0,
+        lockAmount: 0,
+      };
+      (basic as any).block = {
+        height: 1,
+      };
+
+      const locked = await basic.lock(height, amount);
+      expect(locked).toBe('Invalid height or amount');
       done();
     });
   });
