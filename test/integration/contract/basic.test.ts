7781abbbbee99f7086b621029ceb803edfbeaf64
xpgeng <xpgeng.me@gmail.com>
2019-05-06 17:16:04 +0800

    test: add more checkings to the integration tests for contract/basic

--- test/integration/contract/basic.test.ts
+++ test/integration/contract/basic.test.ts
@@ -29,70 +29,124 @@ describe('basic', () => {
   }, lib.oneMinute);
 
   describe('transfer', () => {
-    it('should transfer to a recipient account', async done => {
-      const amount = 5 * 1e8;
-      const recipient = 'GuQr4DM3aiTD36EARqDpbfsEHoNF';
-      const message = '';
+    it(
+      'should transfer to a recipient account',
+      async () => {
+        const amount = 5 * 1e8;
+        const recipient = 'GuQr4DM3aiTD36EARqDpbfsEHoNF';
+        const message = '';
 
-      const trs = gnyJS.basic.transfer(
-        recipient,
-        amount,
-        message,
-        genesisSecret
-      );
-      const transData = {
-        transaction: trs,
-      };
+        // Check the balance before the transaction
+        const beforeTrs = await axios.get(
+          'http://localhost:4096/api/accounts/getBalance?address=' + recipient
+        );
+        expect(beforeTrs.data.balances[0].gny).toBe(0);
+        await lib.onNewBlock();
 
-      const { data } = await axios.post(
-        'http://localhost:4096/peer/transactions',
-        transData,
-        config
-      );
-      expect(data).toHaveProperty('transactionId');
-      done();
-    });
+        // Transaction
+        const trs = gnyJS.basic.transfer(
+          recipient,
+          amount,
+          message,
+          genesisSecret
+        );
+        const transData = {
+          transaction: trs,
+        };
+
+        const { data } = await axios.post(
+          'http://localhost:4096/peer/transactions',
+          transData,
+          config
+        );
+        expect(data).toHaveProperty('transactionId');
+
+        await lib.onNewBlock();
+
+        // Check the balance after the transaction
+        const afterTrs = await axios.get(
+          'http://localhost:4096/api/accounts/getBalance?address=' + recipient
+        );
+        expect(afterTrs.data.balances[0].gny).toBe(String(5 * 1e8));
+      },
+      lib.oneMinute
+    );
   });
 
   describe('setUserName', () => {
-    it('should set the user name', async done => {
-      const username = 'xpgeng';
+    it(
+      'should set the user name',
+      async () => {
+        const username = 'xpgeng';
 
-      const trs = gnyJS.basic.setUserName(username, genesisSecret);
-      const transData = {
-        transaction: trs,
-      };
+        const beforeset = await axios.get(
+          'http://localhost:4096/api/accounts?username=' + username
+        );
+        expect(beforeset.data).toHaveLength(0);
+        await lib.onNewBlock();
 
-      const { data } = await axios.post(
-        'http://localhost:4096/peer/transactions',
-        transData,
-        config
-      );
+        const trs = gnyJS.basic.setUserName(username, genesisSecret);
+        const transData = {
+          transaction: trs,
+        };
 
-      expect(data).toHaveProperty('success');
-      expect(data).toHaveProperty('transactionId');
-      done();
-    });
+        const { data } = await axios.post(
+          'http://localhost:4096/peer/transactions',
+          transData,
+          config
+        );
+
+        expect(data).toHaveProperty('success');
+        expect(data).toHaveProperty('transactionId');
+
+        await lib.onNewBlock();
+        const afterset = await axios.get(
+          'http://localhost:4096/api/accounts?username=' + username
+        );
+        expect(afterset.data.username).toBe(username);
+      },
+      lib.oneMinute
+    );
   });
 
   describe('lock', () => {
-    it('should lock the sender with an amount according to the height', async done => {
-      const trs = gnyJS.basic.lock(173000, 30 * 1e8, genesisSecret);
+    it(
+      'should lock the sender with an amount according to the height',
+      async () => {
+        // Before lock
+        const keys = gnyJS.crypto.getKeys(genesisSecret);
+        const senderId = gnyJS.crypto.getAddress(keys.publicKey);
+        const beforeLock = await axios.get(
+          'http://localhost:4096/api/accounts?address=' + senderId
+        );
+        expect(beforeLock.data.account.lockHeight).toBe('0');
+        await lib.onNewBlock();
 
-      const transData = {
-        transaction: trs,
-      };
+        // lock
+        const trs = gnyJS.basic.lock(173000, 30 * 1e8, genesisSecret);
 
-      const { data } = await axios.post(
-        'http://localhost:4096/peer/transactions',
-        transData,
-        config
-      );
+        const transData = {
+          transaction: trs,
+        };
 
-      expect(data).toHaveProperty('success');
-      expect(data).toHaveProperty('transactionId');
-      done();
-    });
+        const { data } = await axios.post(
+          'http://localhost:4096/peer/transactions',
+          transData,
+          config
+        );
+
+        expect(data).toHaveProperty('success');
+        expect(data).toHaveProperty('transactionId');
+
+        // After lock
+        await lib.onNewBlock();
+        const afterLock = await axios.get(
+          'http://localhost:4096/api/accounts?address=' + trs.senderId
+        );
+        expect(afterLock.data.account.lockHeight).toBe(String(173000));
+      },
+      lib.oneMinute
+    );
   });
 
   describe('unlock', () => {
@@ -109,9 +163,16 @@ describe('basic', () => {
           lockTransData,
           config
         );
-
         await lib.onNewBlock();
 
+        // After lock
+        const afterLock = await axios.get(
+          'http://localhost:4096/api/accounts?address=' + lockTrs.senderId
+        );
+        expect(afterLock.data.account.lockHeight).toBe(String(173000));
+        await lib.onNewBlock();
+
+        // unlock
         const trs = gnyJS.basic.unlock(genesisSecret);
         const transData = {
           transaction: trs,
@@ -148,9 +209,16 @@ describe('basic', () => {
           nameTransData,
           config
         );
-
         await lib.onNewBlock();
 
+        // Before register
+        const beforeRegister = await axios.get(
+          'http://localhost:4096/api/accounts?username=' + username
+        );
+        expect(beforeRegister.data.isDelegate).toBe(0);
+        await lib.onNewBlock();
+
+        // Register the delegate
         const trs = gnyJS.basic.registerDelegate(genesisSecret);
 
         const transData = {
@@ -165,12 +233,19 @@ describe('basic', () => {
 
         expect(data).toHaveProperty('success');
         expect(data).toHaveProperty('transactionId');
+        await lib.onNewBlock();
+
+        // After register
+        const afterRegister = await axios.get(
+          'http://localhost:4096/api/accounts?username=' + username
+        );
+        expect(afterRegister.data.isDelegate).toBe(1);
       },
       lib.oneMinute
     );
 
     it(
-      'should return error if usename is not set',
+      'should return error if username is not set',
       async () => {
         const trs = gnyJS.basic.registerDelegate(genesisSecret);
 
@@ -234,6 +309,13 @@ describe('basic', () => {
         );
         await lib.onNewBlock();
 
+        // Before vote
+        const beforeVote = await axios.get(
+          'http://localhost:4096/api/accounts/getVotes?username=' + username
+        );
+        expect(beforeVote.data.delegates).toHaveLength(0);
+        await lib.onNewBlock();
+
         const trs = gnyJS.basic.vote(['xpgeng'], genesisSecret);
 
         const transData = {
@@ -248,6 +330,13 @@ describe('basic', () => {
 
         expect(data).toHaveProperty('success');
         expect(data).toHaveProperty('transactionId');
+
+        await lib.onNewBlock();
+        // After vote
+        const afterVote = await axios.get(
+          'http://localhost:4096/api/accounts/getVotes?username=' + username
+        );
+        expect(afterVote.data.delegates).toHaveLength(1);
       },
       lib.oneMinute
     );
@@ -305,6 +394,14 @@ describe('basic', () => {
         );
         await lib.onNewBlock();
 
+        // After vote
+        const afterVote = await axios.get(
+          'http://localhost:4096/api/accounts/getVotes?username=' + username
+        );
+        expect(afterVote.data.delegates).toHaveLength(1);
+        await lib.onNewBlock();
+
+        // Unvote
         const trs = gnyJS.basic.unvote(['xpgeng'], genesisSecret);
 
         const transData = {
@@ -319,6 +416,13 @@ describe('basic', () => {
 
         expect(data).toHaveProperty('success');
         expect(data).toHaveProperty('transactionId');
+        await lib.onNewBlock();
+
+        // After unvote
+        const afterUnvote = await axios.get(
+          'http://localhost:4096/api/accounts/getVotes?username=' + username
+        );
+        expect(afterUnvote.data.delegates).toHaveLength(0);
       },
       lib.oneMinute
     );
