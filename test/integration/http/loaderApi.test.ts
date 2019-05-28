e989a9ccfa705ad98b4945c1115e8ac50f183797
xpgeng <xpgeng.me@gmail.com>
2019-05-28 20:19:33 +0800

    test: add integration tests for http/api/loaderApi

---
+++ test/integration/http/loaderApi.test.ts
@@ -0,0 +1,52 @@
+import * as lib from '../lib';
+import axios from 'axios';
+
+describe('blocksApi', () => {
+  beforeAll(async done => {
+    await lib.deleteOldDockerImages();
+    await lib.buildDockerImage();
+    done();
+  }, lib.tenMinutes);
+
+  beforeEach(async done => {
+    await lib.spawnContainer();
+    done();
+  }, lib.oneMinute);
+
+  afterEach(async done => {
+    await lib.stopAndKillContainer();
+    done();
+  }, lib.oneMinute);
+
+  describe('/status', () => {
+    it(
+      'should get the number of delegates',
+      async done => {
+        const { data } = await axios.get(
+          'http://localhost:4096/api/loader/status'
+        );
+        expect(data).toHaveProperty('loaded');
+        expect(data).toHaveProperty('lastBlockHeight');
+        expect(data).toHaveProperty('count');
+        done();
+      },
+      lib.oneMinute
+    );
+  });
+
+  describe('/status/sync', () => {
+    it(
+      'should get all the delegates',
+      async done => {
+        const { data } = await axios.get(
+          'http://localhost:4096/api/loader/status/sync'
+        );
+        expect(data).toHaveProperty('syncing');
+        expect(data).toHaveProperty('blocks');
+        expect(data).toHaveProperty('height');
+        done();
+      },
+      lib.oneMinute
+    );
+  });
+});
