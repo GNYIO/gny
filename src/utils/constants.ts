ede9f174c84017430a79bc9c502897f6cf729274
a1300 <matthias-knopp@gmx.net>
2019-06-07 17:42:45 +0200

    chore: make constants better visible, #103

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const maxAmount: number = 100000000;
-export const maxPayloadLength: number = 8 * 1024 * 1024;
+export const MAX_PAYLOAD_LENGTH: number = 8 * 1024 * 1024;
 export const blockHeaderLength: number = 248;
 export const addressLength: number = 208;
 export const maxAddressesLength: number = 208 * 128;
@@ -15,7 +15,7 @@ export const maxConfirmations: number = 77 * 100;
 export const confirmationLength: number = 77;
 export const fixedPoint: number = 10 ** 6;
 export const totalAmount: number = 2500000000000000;
-export const maxTxsPerBlock: number = 20000;
+export const MAX_TXS_PER_BLOCK: number = 20000;
 export const TIMEOUT: number = 15;
 
 export const INTERVAL: number = 10;
