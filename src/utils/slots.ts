5812663a665d67674148b5a9d9efb344a28bff3d
xpgeng <xpgeng.me@gmail.com>
2019-06-20 16:10:43 +0800

    fix: cannot get class instances using this

--- src/utils/slots.ts
+++ src/utils/slots.ts
@@ -4,45 +4,45 @@ class Slots {
   public delegates = DELEGATES;
   constructor() {}
 
-  getEpochTime(time: number | undefined) {
+  getEpochTime = (time: number | undefined) => {
     if (time === undefined) {
       time = Date.now();
     }
     return Math.floor((time - EPOCH_TIME.getTime()) / 1000);
-  }
+  };
 
-  getTime(time: number | undefined) {
+  getTime = (time: number | undefined) => {
     return this.getEpochTime(time);
-  }
+  };
 
-  getRealTime(epochTime) {
+  getRealTime = epochTime => {
     if (epochTime === undefined) {
       epochTime = this.getTime(undefined);
     }
 
     const start = Math.floor(EPOCH_TIME.getTime() / 1000) * 1000;
     return start + epochTime * 1000;
-  }
+  };
 
-  getSlotNumber(epochTime?: number) {
+  getSlotNumber = (epochTime?: number) => {
     if (epochTime === undefined) {
       epochTime = this.getTime(undefined);
     }
 
     return Math.floor(epochTime / INTERVAL);
-  }
+  };
 
-  getSlotTime(slot: number) {
+  getSlotTime = (slot: number) => {
     return slot * INTERVAL;
-  }
+  };
 
-  getNextSlot() {
+  getNextSlot = () => {
     return this.getSlotNumber(undefined) + 1;
-  }
+  };
 
-  getLastSlot(nextSlot: number) {
+  getLastSlot = (nextSlot: number) => {
     return nextSlot + DELEGATES;
-  }
+  };
 
   roundTime(date: Date) {
     return Math.floor(date.getTime() / 1000) * 1000;
