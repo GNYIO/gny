85184780273435989900cc69ae88e8f28d12cb3e
a1300 <matthias-knopp@gmx.net>
2019-06-30 19:41:46 +0200

    chore: finish ConsensusHelper unit tests, #103

--- test/unit/core/ConsensusHelper.test.ts
+++ test/unit/core/ConsensusHelper.test.ts
@@ -1,9 +1,4 @@
-import {
-  IBlock,
-  KeyPair,
-  ManyVotes,
-  Transaction,
-} from '../../../src/interfaces';
+import { IBlock, ManyVotes, Transaction } from '../../../src/interfaces';
 import { ConsensusHelper } from '../../../src/core/ConsensusHelper';
 import * as ed from '../../../src/utils/ed';
 import * as crypto from 'crypto';
@@ -13,7 +8,8 @@ import { ConsensusBase } from '../../../src/base/consensus';
 
 function createRandomBlock(height: number = 6, prevBlockId = randomHex(32)) {
   const keyPair = randomKeyPair();
-  const timestamp = slots.getSlotNumber(slots.getSlotNumber());
+  const timestamp = slots.getSlotTime(slots.getSlotNumber());
+
   const lastBlock = {
     id: prevBlockId,
     height: height - 1,
@@ -94,7 +90,7 @@ describe('ConsensusHelper', () => {
       done();
     });
 
-    it.only('addPendingVotes() - calling function with votes that have not the same block "id" as the pendingBlock are not getting added', done => {
+    it('addPendingVotes() - calling function with votes that have not the same block "id" as the pendingBlock are not getting added', done => {
       // preparation block 1
       const block1 = {
         height: 1,
@@ -127,8 +123,27 @@ describe('ConsensusHelper', () => {
       done();
     });
 
-    it.skip('addingPendingVotes() - adding twice the same vote from a delegate (publicKey) has no effect on pendingVotes signatures', done => {
+    it('addingPendingVotes() - adding twice the same vote from a delegate (publicKey) has no effect on pendingVotes signatures', done => {
       // preparation
+      const pendingBlock = createRandomBlock(1);
+      let state = BlocksHelper.getInitialState();
+      state = ConsensusHelper.setPendingBlock(state, pendingBlock);
+
+      const keypairs = [randomKeyPair()];
+      const votes = ConsensusBase.createVotes(keypairs, pendingBlock);
+
+      // first add
+      state = ConsensusHelper.addPendingVotes(state, votes);
+
+      // check state
+      expect(state.pendingVotes.signatures).toHaveLength(1);
+
+      // second add (with exact same votes)
+      const result = ConsensusHelper.addPendingVotes(state, votes);
+
+      expect(result.pendingVotes.signatures).toHaveLength(1);
+
+      done();
     });
 
     it('addPendingVotes() - adds pendingVotes if no pendingVotes are available yet', done => {
@@ -186,18 +201,151 @@ describe('ConsensusHelper', () => {
     });
   });
 
-  describe.skip('hasPendingBlock', () => {
-    it.skip('should return false when remote votes are not enough ', done => {
-      const existed = ConsensusBase.hasPendingBlock(timestamp);
-      expect(existed).toBeFalsy();
+  describe('setPendingBlock', () => {
+    it('setPendingBlock() - sets pendingBlock and returns other state object reference', done => {
+      const state = BlocksHelper.getInitialState();
+      const pendingBlock = createRandomBlock(3);
+
+      const result = ConsensusHelper.setPendingBlock(state, pendingBlock);
+
+      expect(result.pendingBlock).toEqual(pendingBlock);
+      expect(result).not.toBe(state); // other object reference gets returned
+
+      done();
+    });
+  });
+
+  describe('hasPendingBlock', () => {
+    it('hasPendingBlock() - returns false if state has no pendingBlock', done => {
+      const state = BlocksHelper.getInitialState();
+      const timestamp = Date.now();
+
+      const result = ConsensusHelper.hasPendingBlock(state, timestamp);
+
+      expect(result).toEqual(false);
+
       done();
     });
 
-    it.skip('should return false when remote votes are not enough ', done => {
-      ConsensusBase.pendingBlock = block;
-      const existed = ConsensusBase.hasPendingBlock(timestamp);
-      expect(existed).toBeTruthy();
+    it('hasPendingBlock() - returns true if state has pendingBlock and currentTimestamp are in the same slot', done => {
+      // preparation
+      let state = BlocksHelper.getInitialState();
+      const pendingBlock = createRandomBlock(1);
+
+      // make test not brittle, block.timestamp and currentTimestamp should be the same
+      const nowInEpochTime = slots.getEpochTime(undefined);
+      pendingBlock.timestamp = nowInEpochTime;
+      state = ConsensusHelper.setPendingBlock(state, pendingBlock);
+
+      // pre check
+      expect(state.pendingBlock.timestamp).toEqual(nowInEpochTime);
+
+      // act
+      const result = ConsensusHelper.hasPendingBlock(state, nowInEpochTime);
+      expect(result).toEqual(true);
+
       done();
+    });
+
+    it('hasPendingBlock() - returns false if timestamp of pendingBlock was 10 seconds before current timestamp', done => {
+      let state = BlocksHelper.getInitialState();
+      const pendingBlock = createRandomBlock(1);
+
+      // make test not brittle, block.timestamp and currentTimestamp should originate from same timestamp
+      // pendingBlock.timestamp is 10 seconds before "nowInEpochTime" variable
+      const currentTimestamp = Date.now();
+      const oldTimestamp = currentTimestamp - 10000;
+
+      const currentEpochTime = slots.getEpochTime(currentTimestamp);
+      const oldEpochTime = slots.getEpochTime(oldTimestamp);
+
+      pendingBlock.timestamp = oldEpochTime; // pendingBlock was 10 seconds before
+
+      // pre check
+      const slot1 = slots.getSlotNumber(currentEpochTime);
+      const slot2 = slots.getSlotNumber(pendingBlock.timestamp);
+      expect(slot1).toEqual(slot2 + 1);
+
+      // prepare state
+      state = ConsensusHelper.setPendingBlock(state, pendingBlock);
+      // pre check: state has pendingBlock
+      expect(state.pendingBlock).not.toBeUndefined();
+
+      // act
+      const result = ConsensusHelper.hasPendingBlock(state, currentEpochTime);
+
+      expect(result).toEqual(false);
+      done();
+    });
+  });
+
+  describe('getPendingBlock', () => {
+    it('getPendingBlock() - returns no pendingBlock if there is not one', done => {
+      const state = BlocksHelper.getInitialState();
+
+      const result = ConsensusHelper.getPendingBlock(state);
+      expect(result).toBeUndefined();
+
+      done();
+    });
+
+    it('getPendingBlock() - returns pendingblock if there is one', done => {
+      let state = BlocksHelper.getInitialState();
+      const pendingBlock = createRandomBlock(1);
+      state = ConsensusHelper.setPendingBlock(state, pendingBlock);
+
+      const result = ConsensusHelper.getPendingBlock(state);
+      expect(result).toEqual(pendingBlock);
+
+      done();
+    });
+  });
+
+  describe('clearState', () => {
+    it('clearState() - resets pendingBlock, pendingVotes and votesKeySet', done => {
+      // preparation
+      let state = BlocksHelper.getInitialState();
+
+      // set pendingBlock
+      const pendingBlock = createRandomBlock(1);
+      state = ConsensusHelper.setPendingBlock(state, pendingBlock);
+
+      // add pendingVotes
+      const keypairs = [randomKeyPair(), randomKeyPair(), randomKeyPair()];
+      const pendingVotes = ConsensusBase.createVotes(keypairs, pendingBlock);
+      state = ConsensusHelper.addPendingVotes(state, pendingVotes);
+
+      // pre check
+      expect(state.pendingBlock).not.toBeUndefined();
+      expect(state.pendingVotes).not.toBeUndefined();
+      expect(state.pendingVotes.signatures).toHaveLength(3);
+      expect(Object.keys(state.votesKeySet).length).toEqual(3);
+
+      // act
+      const result = ConsensusHelper.clearState(state);
+
+      // after check
+      expect(result.pendingBlock).toBeUndefined();
+      expect(result.pendingVotes).toBeUndefined();
+      expect(Object.keys(result.votesKeySet).length).toEqual(0);
+
+      done();
+    });
+
+    describe('CollectingVotes', () => {
+      it('CollectingVotes() - set the privIsCollectingVotes prop to true', done => {
+        const state = BlocksHelper.getInitialState();
+
+        // pre check
+        expect(state.privIsCollectingVotes).toEqual(false);
+
+        const result = ConsensusHelper.CollectingVotes(state);
+
+        expect(result.privIsCollectingVotes).toEqual(true);
+        expect(result).not.toBe(state); // returned state is is other object reference
+
+        done();
+      });
     });
   });
 });
