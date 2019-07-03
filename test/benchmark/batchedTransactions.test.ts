3f8ef300e248fbc34bd0291f1956d97cfd46c07b
xpgeng <xpgeng.me@gmail.com>
2019-07-03 19:49:25 +0800

    test: modify benchmark tests without reporting connection errors

--- test/benchmark/batchedTransactions.test.ts
+++ test/benchmark/batchedTransactions.test.ts
@@ -1,39 +1,17 @@
+/**
+ * @jest-environment node
+ */
 import * as gnyJS from '../../packages/gny-js';
 import * as crypto from 'crypto';
 import * as bs58 from 'bs58';
 import axios from 'axios';
+import * as lib from '../integration/lib';
 
 import si = require('systeminformation');
 import Benchmark = require('benchmark');
 
 async function onStart() {
-  console.log('Benchmark for batch transactions started...');
-}
-
-async function onComplete(event) {
-  const sysData = await si.getStaticData();
-
-  const cpu = sysData.cpu;
-  console.log('CPU Information:');
-  console.log('- manufucturer: ' + cpu.manufacturer);
-  console.log('- brand: ' + cpu.brand);
-  console.log('- speed: ' + cpu.speed);
-  console.log('- cores: ' + cpu.cores);
-  console.log('- physical cores: ' + cpu.physicalCores);
-  console.log('...');
-
-  const memory = sysData.memLayout;
-  let size = 0;
-  for (const item of memory) {
-    size += item.size;
-  }
-
-  console.log('Memory Information:');
-  console.log('- size: ' + size);
-  console.log('- type: ' + memory[0].type);
-  console.log('...');
-
-  console.log('Benchmark ended...');
+  console.log('Benchmark for single transaction started...');
 }
 
 function onAbort() {
@@ -80,43 +58,91 @@ const config = {
   },
 };
 
-async function batchTransaction() {
-  const test = createTransactions(100);
-  try {
-    const data = {
-      transactions: test,
-    };
+describe('test batch', () => {
+  beforeAll(async done => {
+    await lib.deleteOldDockerImages();
+    await lib.buildDockerImage();
+    done();
+  }, lib.tenMinutes);
 
-    const result = await axios.put(
-      'http://localhost:4096/api/transactions/batch',
-      data,
-      config
-    );
-    // console.log(JSON.stringify(result.data, null, 2));
-  } catch (e) {
-    console.log(e);
-    // console.log(JSON.stringify(e.response.data, null, 2));
-  }
-}
+  beforeEach(async done => {
+    await lib.spawnContainer();
+    done();
+  }, lib.oneMinute);
 
-const suite = new Benchmark.Suite('BatchTransactions', {
-  // called when the suite starts running
-  onStart: onStart,
+  afterEach(async done => {
+    await lib.stopAndKillContainer();
+    done();
+  }, lib.oneMinute);
 
-  // called between running benchmarks
-  onCycle: function(event) {
-    console.log(String(event.target));
-  },
+  it(
+    'batch transactions',
+    done => {
+      async function onComplete(event) {
+        console.log(
+          'Benchmark for batch transactions: ' + event.target.hz + ' ops/sec'
+        );
+        const sysData = await si.getStaticData();
 
-  // called when aborted
-  onAbort: onAbort,
+        const cpu = sysData.cpu;
+        console.log('CPU Information:');
+        console.log('- manufucturer: ' + cpu.manufacturer);
+        console.log('- brand: ' + cpu.brand);
+        console.log('- speed: ' + cpu.speed);
+        console.log('- cores: ' + cpu.cores);
+        console.log('- physical cores: ' + cpu.physicalCores);
+        console.log('...');
 
-  // called when a test errors
-  onError: onError,
+        const memory = sysData.memLayout;
+        let size = 0;
+        for (const item of memory) {
+          size += item.size;
+        }
+        console.log('Memory Information:');
+        console.log('- size: ' + size);
+        console.log('- type: ' + memory[0].type);
+        console.log('...');
 
-  // called when the suite completes running
-  onComplete: onComplete,
+        console.log('Benchmark ended...');
+        done();
+      }
+      const suite = new Benchmark.Suite('BatchTransaction', {
+        // called when the suite starts running
+        onStart: onStart,
+
+        // called between running benchmarks
+        onCycle: function(event) {
+          console.log(String(event.target));
+        },
+
+        // called when aborted
+        onAbort: onAbort,
+
+        // called when a test errors
+        onError: onError,
+
+        // called when the suite completes running
+        onComplete: onComplete,
+      });
+
+      // add tests
+      suite.add('BatchTransactions', {
+        defer: true,
+        fn: async function(deferred) {
+          const trs = createTransactions(10);
+          const data = {
+            transactions: trs,
+          };
+          await axios.put(
+            'http://localhost:4096/api/transactions/batch',
+            data,
+            config
+          );
+          deferred.resolve();
+        },
+      });
+      suite.run();
+    },
+    lib.oneMinute
+  );
 });
-suite.add('BatchTransactions', batchTransaction);
-
-suite.run({ async: true });
--- test/benchmark/singleTransactions.test.ts
+++ test/benchmark/singleTransactions.test.ts
@@ -1,39 +1,17 @@
+/**
+ * @jest-environment node
+ */
 import * as gnyJS from '../../packages/gny-js';
 import * as crypto from 'crypto';
 import * as bs58 from 'bs58';
 import axios from 'axios';
+import * as lib from '../integration/lib';
 
 import si = require('systeminformation');
 import Benchmark = require('benchmark');
 
 async function onStart() {
   console.log('Benchmark for single transaction started...');
-}
-
-async function onComplete(event) {
-  const sysData = await si.getStaticData();
-
-  const cpu = sysData.cpu;
-  console.log('CPU Information:');
-  console.log('- manufucturer: ' + cpu.manufacturer);
-  console.log('- brand: ' + cpu.brand);
-  console.log('- speed: ' + cpu.speed);
-  console.log('- cores: ' + cpu.cores);
-  console.log('- physical cores: ' + cpu.physicalCores);
-  console.log('...');
-
-  const memory = sysData.memLayout;
-  let size = 0;
-  for (const item of memory) {
-    size += item.size;
-  }
-
-  console.log('Memory Information:');
-  console.log('- size: ' + size);
-  console.log('- type: ' + memory[0].type);
-  console.log('...');
-
-  console.log('Benchmark ended...');
 }
 
 function onAbort() {
@@ -80,31 +58,92 @@ const config = {
   },
 };
 
-async function singleTransactions() {
-  const test = createTransactions(10);
-  try {
-    for (const trs of test) {
-      const data = {
-        transaction: trs,
-      };
+describe('test single', () => {
+  beforeAll(async done => {
+    await lib.deleteOldDockerImages();
+    await lib.buildDockerImage();
+    done();
+  }, lib.tenMinutes);
 
-      await axios.post('http://localhost:4096/peer/transactions', data, config);
-    }
-  } catch (e) {
-    console.log(e);
-    // console.log(JSON.stringify(e.response.data, null, 2));
-  }
-}
+  beforeEach(async done => {
+    await lib.spawnContainer();
+    done();
+  }, lib.oneMinute);
 
-const suite = new Benchmark.Suite('SingleTransaction', {
-  onStart: onStart,
-  onCycle: function(event) {
-    console.log(String(event.target));
-  },
-  onAbort: onAbort,
-  onError: onError,
-  onComplete: onComplete,
+  afterEach(async done => {
+    await lib.stopAndKillContainer();
+    done();
+  }, lib.oneMinute);
+
+  it(
+    'test single',
+    done => {
+      async function onComplete(event) {
+        console.log(
+          'Benchmark for single transaction: ' + event.target.hz + ' ops/sec'
+        );
+        const sysData = await si.getStaticData();
+
+        const cpu = sysData.cpu;
+        console.log('CPU Information:');
+        console.log('- manufucturer: ' + cpu.manufacturer);
+        console.log('- brand: ' + cpu.brand);
+        console.log('- speed: ' + cpu.speed);
+        console.log('- cores: ' + cpu.cores);
+        console.log('- physical cores: ' + cpu.physicalCores);
+        console.log('...');
+
+        const memory = sysData.memLayout;
+        let size = 0;
+        for (const item of memory) {
+          size += item.size;
+        }
+        console.log('Memory Information:');
+        console.log('- size: ' + size);
+        console.log('- type: ' + memory[0].type);
+        console.log('...');
+
+        console.log('Benchmark ended...');
+        done();
+      }
+
+      const suite = new Benchmark.Suite('SingleTransaction', {
+        // called when the suite starts running
+        onStart: onStart,
+
+        // called between running benchmarks
+        onCycle: function(event) {
+          console.log(String(event.target));
+        },
+
+        // called when aborted
+        onAbort: onAbort,
+
+        // called when a test errors
+        onError: onError,
+
+        // called when the suite completes running
+        onComplete: onComplete,
+      });
+
+      // add tests
+      suite.add('SingleTransaction', {
+        defer: true,
+        fn: async function(deferred) {
+          const trs = createTransactions(1);
+          const data = {
+            transaction: trs[0],
+          };
+          await axios.post(
+            'http://localhost:4096/peer/transactions',
+            data,
+            config
+          );
+          deferred.resolve();
+        },
+      });
+      suite.run();
+    },
+    lib.oneMinute
+  );
 });
-suite.add('SingleTransaction', singleTransactions);
-
-suite.run({ async: true });
