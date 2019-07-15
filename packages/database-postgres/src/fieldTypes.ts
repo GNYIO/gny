ce09613070e94993e6245c43d0f7c3c61f263224
a1300 <matthias-knopp@gmx.net>
2019-07-15 19:24:35 +0200

    chore: remove unused enum FieldTypes, #128

--- packages/database-postgres/src/fieldTypes.ts
+++ packages/database-postgres/src/fieldTypes.ts
@@ -1,16 +1,11 @@
-
-export enum FieldTypes {
-  String = 'String',
-  Number = 'Number',
-  BigInt = 'BigInt',
-  Text = 'Text',
-  JSON = 'Json',
-}
-
-
 export class InvalidEntityKeyError extends Error {
   constructor(context, props) {
-    super('Invalid entity key\uff0c( model = ' + context + ", key = '" + JSON.stringify(props) + "' ) ");
+    super(
+      'Invalid entity key\uff0c( model = ' +
+        context +
+        ", key = '" +
+        JSON.stringify(props) +
+        "' ) "
+    );
   }
 }
-
