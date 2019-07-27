637d08a60f3bf0f02f458c432753f9b712536c59
a1300 <matthias-knopp@gmx.net>
2019-07-27 20:02:17 +0200

    chore: remove smartDb test-data file

--- test/integration/orm/smartDB.test.ts
+++ test/integration/orm/smartDB.test.ts
@@ -12,8 +12,8 @@ import {
   IVariable,
   IRound,
   IBalance,
+  IGenesisBlock,
 } from '../../../src/interfaces';
-import { CUSTOM_GENESIS } from './data';
 import { randomBytes } from 'crypto';
 import { generateAddress } from '../../../src/utils/address';
 import { cloneDeep } from 'lodash';
@@ -33,6 +33,23 @@ import {
 import { Round } from '../../../packages/database-postgres/entity/Round';
 import { Transfer } from '../../../packages/database-postgres/entity/Transfer';
 import { Block } from '../../../packages/database-postgres/entity/Block';
+
+const CUSTOM_GENESIS: IGenesisBlock = {
+  version: 0,
+  payloadHash:
+    '4b1598f8e52794520ea65837b44f58b39517cda40548ef6094e5b24c11af3493',
+  timestamp: 0,
+  prevBlockId: null,
+  delegate: 'bb7fc99aae209658bfb1987367e6881cdf648975438abd05aefd16ac214e4f47',
+  height: String(0),
+  count: 0,
+  fees: String(0),
+  reward: String(0),
+  signature:
+    'cf56b32f7e1206bee719ef0cae141beff253b5b93e55b3f9bf7e71705a0f03b4afd8ad53db9aecb32a9054dee5623ee4e85a16fab2c6c75fc17f0263adaefd0c',
+  id: '28d65b4b694b4b4eee7f26cd8653097078b2e576671ccfc51619baf3f07b1541',
+  transactions: [],
+};
 
 const logger: ILogger = {
   log: x => x,
