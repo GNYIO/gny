dafc5d02e98a8c9512bed51ac2cfcab91cd73cad
xpgeng <xpgeng.me@gmail.com>
2019-08-11 16:23:19 +0800

    chore: delete unused functions

--- packages/gny-cli/template/interface/domain.ts
+++
@@ -1,7 +0,0 @@
-app.route.get('/domain/:domain', async function(req) {
-  return await app.model.Domain.findOne({ domain: req.params.domain });
-});
-
-app.route.get('/domain/suffix/:suffix', async function(req) {
-  return await app.model.Domain.findAll({ suffix: req.params.suffix });
-});
