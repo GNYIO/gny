26617eed731162bf2d4a717f9509b960a862471a
xpgeng <xpgeng.me@gmail.com>
2019-09-10 23:25:31 +0800

    chore: integrate basic api #151

---
+++ packages/gny-client/api/basic.ts
@@ -0,0 +1,38 @@
+import { Connection } from '../connection';
+import axios from 'axios';
+
+export class Basic {
+  public constructor(protected readonly connection: Connection) {}
+
+  public async get(url: string, params?: any) {
+    try {
+      const { data, headers, status } = await axios.get(
+        this.connection.baseUrl + url,
+        { params: params }
+      );
+      return {
+        data,
+        headers,
+        status,
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async post(url: string, params?: any) {
+    try {
+      const { data, headers, status } = await axios.post(
+        this.connection.baseUrl + url,
+        params
+      );
+      return {
+        data,
+        headers,
+        status,
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
