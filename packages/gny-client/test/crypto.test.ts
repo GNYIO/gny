25ed640b66c15a4c278b144e383958cce8c0cb7d
xpgeng <xpgeng.me@gmail.com>
2019-09-15 01:30:51 +0800

    test: modify tests of /crypto

--- packages/gny-client/test/crypto.test.ts
+++ packages/gny-client/test/crypto.test.ts
@@ -1,5 +1,6 @@
-import should = require('should');
-import gnyClient = require('../index.js');
+import 'jest-extended';
+import * as gnyClient from '../index';
+import extendedJoi from '../../../src/utils/extendedJoi';
 
 describe('crypto.js', () => {
   const crypto = gnyClient.crypto;
@@ -36,12 +37,8 @@ describe('crypto.js', () => {
     bytes = null;
   });
 
-  it('should be ok', () => {
-    crypto.should.be.ok;
-  });
-
   it('should be object', () => {
-    crypto.should.be.type('object');
+    expect(crypto).toBeObject();
   });
 
   it('should has properties', () => {
@@ -59,28 +56,20 @@ describe('crypto.js', () => {
       'fixedPoint',
     ];
     properties.forEach(function(property) {
-      crypto.should.have.property(property);
+      expect(crypto).toHaveProperty(property);
     });
   });
 
   describe('#getBytes', () => {
-    it('should be ok', () => {
-      getBytes.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getBytes.should.be.type('function');
-    });
-
-    it('should return Buffer of simply transaction and buffer most be 165 length', () => {
+    it('should return Buffer of simply transaction and buffer most be 155 length', () => {
       const transaction = {
         type: 1,
         timestamp: 68365026,
         fee: 10000000,
-        args: [200000000000, 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'],
+        args: [200000000000, 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv'],
         senderPublicKey:
           '116025d5664ce153b02c69349798ab66144edd2a395e822b13587780ac9c9c09',
-        senderId: 'ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5',
+        senderId: 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv',
         signatures: [
           '6b9a02208c0c36a0adf527d117cc3840f4b6c0a6eeac8cd212635f120592a101b68aeae0efe43ee28b0fd2add6fdb14f6b51b5ebff467b02909f4cdd66276603',
         ],
@@ -88,20 +77,19 @@ describe('crypto.js', () => {
       };
 
       bytes = getBytes(transaction);
-      should(bytes).be.ok;
-      should(bytes).be.type('object');
-      should(bytes.length).be.equal(165);
+      expect(bytes).toBeObject();
+      expect(bytes.length).toEqual(155);
     });
 
-    it('should return Buffer of transaction with second signature and buffer most be 229 length', () => {
+    it('should return Buffer of transaction with second signature and buffer most be 219 length', () => {
       const transaction = {
         type: 1,
         timestamp: 68365928,
         fee: 10000000,
-        args: [200000000000, 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'],
+        args: [200000000000, 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv'],
         senderPublicKey:
           '116025d5664ce153b02c69349798ab66144edd2a395e822b13587780ac9c9c09',
-        senderId: 'ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5',
+        senderId: 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv',
         signatures: [
           'e1da25b8278d9cf4dcb32d8be73681ce701f0f01518b3d68e80c235a0f9d7d3fcfae7d454976f82535bc181fccd0f7cefc2fe3f1f158e76789a775bdce1d2906',
         ],
@@ -111,27 +99,18 @@ describe('crypto.js', () => {
       };
 
       bytes = getBytes(transaction);
-      should(bytes).be.ok;
-      should(bytes).be.type('object');
-      should(bytes.length).be.equal(229);
+      expect(bytes).toBeObject();
+      expect(bytes.length).toEqual(219);
     });
   });
 
   describe('#getHash', () => {
-    it('should be ok', () => {
-      getHash.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getHash.should.be.type('function');
-    });
-
     it('should return Buffer and Buffer must be 32 bytes length', () => {
       const transaction = {
         type: 1,
         timestamp: 68365026,
         fee: 10000000,
-        args: [200000000000, 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'],
+        args: [200000000000, 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv'],
         senderPublicKey:
           '116025d5664ce153b02c69349798ab66144edd2a395e822b13587780ac9c9c09',
         senderId: 'ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5',
@@ -141,27 +120,18 @@ describe('crypto.js', () => {
       };
 
       const result = getHash(transaction);
-      should(result).be.ok;
-      should(result).be.type('object');
-      should(result.length).be.equal(32);
+      expect(result).toBeObject();
+      expect(result.length).toEqual(32);
     });
   });
 
   describe('#getId', () => {
-    it('should be ok', () => {
-      getId.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getId.should.be.type('function');
-    });
-
     it('should return string id and be equal to 43a9ed49665d7e905b717...', () => {
       const transaction = {
         type: 1,
         timestamp: 68365026,
         fee: 10000000,
-        args: [200000000000, 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'],
+        args: [200000000000, 'G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv'],
         senderPublicKey:
           '116025d5664ce153b02c69349798ab66144edd2a395e822b13587780ac9c9c09',
         senderId: 'ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5',
@@ -171,157 +141,111 @@ describe('crypto.js', () => {
       };
 
       const id = getId(transaction);
-      id.should.be
-        .type('string')
-        .and.equal(
-          '43a9ed49665d7e905b717e54f39b1d9976e3332608bacf6083a3e130a0d12eed'
-        );
+      expect(id).toBeString();
+      expect(id).toEqual(
+        '07152e2898a19b1547de48f81d92ed696b33ff9fe00f7abd1bfbe40b39e521aa'
+      );
     });
   });
 
   describe('#getFee', () => {
-    it('should be ok', () => {
-      getFee.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getFee.should.be.type('function');
-    });
-
     it('should return number', () => {
       const fee = getFee({ amount: 100000, type: 0 });
-      fee.should.be.type('number');
-      fee.should.be.not.NaN;
+      expect(fee).toBeNumber();
+      expect(fee).not.toBeNull();
     });
 
     it('should return 10000000', () => {
       const fee = getFee({ amount: 100000, type: 0 });
-      fee.should.be.type('number').and.equal(10000000);
+      expect(fee).toBeNumber();
+      expect(fee).toEqual(10000000);
     });
 
     it('should return 10000000000', () => {
       const fee = getFee({ type: 1 });
-      fee.should.be.type('number').and.equal(10000000000);
+      expect(fee).toBeNumber();
+      expect(fee).toEqual(10000000000);
     });
 
     it('should be equal 1000000000000', () => {
       const fee = getFee({ type: 2 });
-      fee.should.be.type('number').and.equal(1000000000000);
+      expect(fee).toBeNumber();
+      expect(fee).toEqual(1000000000000);
     });
 
     it('should be equal 100000000', () => {
       const fee = getFee({ type: 3 });
-      fee.should.be.type('number').and.equal(100000000);
+      expect(fee).toBeNumber();
+      expect(fee).toEqual(100000000);
     });
   });
 
   describe('fixedPoint', () => {
-    it('should be ok', () => {
-      fixedPoint.should.be.ok;
-    });
-
-    it('should be number', () => {
-      fixedPoint.should.be.type('number').and.not.NaN;
-    });
-
-    it('should be equal 100000000', () => {
-      fixedPoint.should.be.equal(100000000);
+    it('should be number and equal to 100000000', () => {
+      expect(fixedPoint).toBeNumber();
+      expect(fixedPoint).toEqual(100000000);
     });
   });
 
   describe('#sign', () => {
-    it('should be ok', () => {
-      sign.should.be.ok;
-    });
-
     it('should be a function', () => {
-      sign.should.be.type('function');
+      expect(sign).toBeFunction();
     });
   });
 
   describe('#secondSign', () => {
-    it('should be ok', () => {
-      secondSign.should.be.ok;
-    });
-
     it('should be a function', () => {
-      secondSign.should.be.type('function');
+      expect(secondSign).toBeFunction();
     });
   });
 
   describe('#getKeys', () => {
-    it('should be ok', () => {
-      getKeys.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getKeys.should.be.type('function');
-    });
-
     it('should return two keys in hex', () => {
       const keys = getKeys('secret');
 
-      keys.should.be.ok;
-      keys.should.be.type('object');
-      keys.should.have.property('publicKey');
-      keys.should.have.property('privateKey');
-      keys.publicKey.should.be.type('string').and.match(() => {
-        try {
-          new Buffer(keys.publicKey, 'hex');
-        } catch (e) {
-          return false;
-        }
+      expect(keys).toBeObject();
+      expect(keys).toHaveProperty('publicKey');
+      expect(keys).toHaveProperty('privateKey');
 
-        return true;
-      });
-      keys.privateKey.should.be.type('string').and.match(() => {
-        try {
-          new Buffer(keys.privateKey, 'hex');
-        } catch (e) {
-          return false;
-        }
+      const publicSchema = extendedJoi
+        .string()
+        .hex(32)
+        .required();
 
-        return true;
-      });
+      const publicReport = extendedJoi.validate(keys.publicKey, publicSchema);
+      expect(publicReport.error).toBeNull();
+
+      const privateSchema = extendedJoi
+        .string()
+        .hex(64)
+        .required();
+      const privateReport = extendedJoi.validate(
+        keys.privateKey,
+        privateSchema
+      );
+      expect(privateReport.error).toBeNull();
     });
   });
 
   describe('#getAddress', () => {
-    it('should be ok', () => {
-      getAddress.should.be.ok;
-    });
-
-    it('should be a function', () => {
-      getAddress.should.be.type('function');
-    });
-
     it('should generate address by publicKey', () => {
       const keys = crypto.getKeys('secret');
       const address = getAddress(keys.publicKey);
 
-      address.should.be.ok;
-      address.should.be.type('string');
-      address.should.be.equal('AFkctfgZFkaATGRhGbj72wzJqACvMyzQ1U');
+      expect(address).toBeString();
+      expect(address).toEqual('G3FkpyJr5gZmd1gPCZ6pQGAM5DPSv');
     });
   });
 
   describe('#verify', () => {
-    it('should be ok', () => {
-      verify.should.be.ok;
-    });
-
     it('should be function', () => {
-      verify.should.be.type('function');
+      expect(verify).toBeFunction();
     });
   });
 
   describe('#verifySecondSignature', () => {
-    it('should be ok', () => {
-      verifySecondSignature.should.be.ok;
-    });
-
     it('should be function', () => {
-      verifySecondSignature.should.be.type('function');
+      expect(verifySecondSignature).toBeFunction();
     });
   });
 });
