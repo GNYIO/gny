a0b859e75a271fa9fc3812cee59d037713ebe306
xpgeng <xpgeng.me@gmail.com>
2019-09-15 01:49:50 +0800

    test: modify tests of /lib/time/slots

--- packages/gny-client/test/slot.test.ts
+++ packages/gny-client/test/slot.test.ts
@@ -1,14 +1,10 @@
-import should = require('should');
+import 'jest-extended';
 
 describe('slots.js', () => {
-  const slots = require('../lib/time/slots.js');
-
-  it('should be ok', () => {
-    slots.should.be.ok;
-  });
+  const slots = require('../lib/time/slots');
 
   it('should be object', () => {
-    slots.should.be.type('object');
+    expect(slots).toBeObject();
   });
 
   it('should have properties', () => {
@@ -23,143 +19,116 @@ describe('slots.js', () => {
       'getLastSlot',
     ];
     properties.forEach(function(property) {
-      slots.should.have.property(property);
+      expect(slots).toHaveProperty(property);
     });
   });
 
   describe('.interval', () => {
     const interval = slots.interval;
 
-    it('should be ok', () => {
-      interval.should.be.ok;
-    });
-
     it('should be number and not NaN', () => {
-      interval.should.be.type('number').and.not.NaN;
+      expect(interval).toBeNumber();
+      expect(interval).not.toBeNull();
     });
   });
 
   describe('.delegates', () => {
     const delegates = slots.delegates;
 
-    it('should be ok', () => {
-      delegates.should.be.ok;
-    });
-
     it('should be number and not NaN', () => {
-      delegates.should.be.type('number').and.not.NaN;
+      expect(delegates).toBeNumber();
+      expect(delegates).not.toBeNull();
     });
   });
 
   describe('#getTime', () => {
     const getTime = slots.getTime;
 
-    it('should be ok', () => {
-      getTime.should.be.ok;
-    });
-
     it('should be a function', () => {
-      getTime.should.be.type('function');
+      expect(getTime).toBeFunction();
     });
 
-    it('should return epoch time as number, equal to 2764800', () => {
-      const d = 1469822400000;
+    it('should return epoch time as number, equal to 27251200', () => {
+      const d = 1569822400000;
       const time = getTime(d);
-      time.should.be.ok;
-      time.should.be.type('number').and.equal(2764800);
+
+      expect(time).toBeNumber();
+      expect(time).toEqual(27251200);
     });
   });
 
   describe('#getRealTime', () => {
     const getRealTime = slots.getRealTime;
 
-    it('should be ok', () => {
-      getRealTime.should.be.ok;
-    });
-
     it('should be a function', () => {
-      getRealTime.should.be.type('function');
+      expect(getRealTime).toBeFunction();
     });
 
-    it('should return return real time, convert 196144 to 1467253744000', () => {
+    it('should return return real time, convert 196144 to 1542767344000', () => {
       const d = 196144;
       const real = getRealTime(d);
-      real.should.be.ok;
-      real.should.be.type('number').and.equal(1467253744000);
+
+      expect(real).toBeNumber();
+      expect(real).toEqual(1542767344000);
     });
   });
 
   describe('#getSlotNumber', () => {
     const getSlotNumber = slots.getSlotNumber;
 
-    it('should be ok', () => {
-      getSlotNumber.should.be.ok;
-    });
-
     it('should be a function', () => {
-      getSlotNumber.should.be.type('function');
+      expect(getSlotNumber).toBeFunction();
     });
 
     it('should return slot number, equal to 19614', () => {
       const d = 196144;
       const slot = getSlotNumber(d);
-      slot.should.be.ok;
-      slot.should.be.type('number').and.equal(19614);
+
+      expect(slot).toBeNumber();
+      expect(slot).toEqual(19614);
     });
   });
 
   describe('#getSlotTime', () => {
     const getSlotTime = slots.getSlotTime;
 
-    it('should be ok', () => {
-      getSlotTime.should.be.ok;
-    });
-
     it('should be function', () => {
-      getSlotTime.should.be.type('function');
+      expect(getSlotTime).toBeFunction();
     });
 
-    it('should return slot time number, equal to ', () => {
+    it('should return slot time number, equal to 196140', () => {
       const slot = 19614;
       const slotTime = getSlotTime(19614);
-      slotTime.should.be.ok;
-      slotTime.should.be.type('number').and.equal(196140);
+      expect(slotTime).toBeNumber();
+      expect(slotTime).toEqual(196140);
     });
   });
 
   describe('#getNextSlot', () => {
     const getNextSlot = slots.getNextSlot;
 
-    it('should be ok', () => {
-      getNextSlot.should.be.ok;
-    });
-
     it('should be function', () => {
-      getNextSlot.should.be.type('function');
+      expect(getNextSlot).toBeFunction();
     });
 
     it('should return next slot number', () => {
       const nextSlot = getNextSlot();
-      nextSlot.should.be.ok;
-      nextSlot.should.be.type('number').and.not.NaN;
+      expect(nextSlot).toBeNumber();
+      expect(nextSlot).not.toBeNull();
     });
   });
 
   describe('#getLastSlot', () => {
     const getLastSlot = slots.getLastSlot;
 
-    it('should be ok', () => {
-      getLastSlot.should.be.ok;
-    });
-
     it('should be function', () => {
-      getLastSlot.should.be.type('function');
+      expect(getLastSlot).toBeFunction();
     });
 
     it('should return last slot number', () => {
       const lastSlot = getLastSlot(slots.getNextSlot());
-      lastSlot.should.be.ok;
-      lastSlot.should.be.type('number').and.not.NaN;
+      expect(lastSlot).toBeNumber();
+      expect(lastSlot).not.toBeNull();
     });
   });
 });
