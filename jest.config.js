1fccf0ebdd84fcdaec84c3e2b4f643cdef6a0fd0
Matthias Knopp <matthias-knopp@gmx.net>
2019-09-18 00:29:42 +0200

    chore: move typesc. config out of jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,30 +9,7 @@ module.exports = {
   globals: {
     'ts-jest': {
       diagnostics: false,
-      tsConfig: {
-        target: 'ES2016',
-        module: 'commonjs',
-        lib: ['es2015', 'es2016', 'es2017'],
-        allowJs: true,
-        checkJs: true,
-        declaration: false,
-        sourceMap: true,
-        strict: false,
-        noImplicitAny: false,
-        strictNullChecks: false,
-        noImplicitThis: false,
-        alwaysStrict: false,
-        noUnusedLocals: true,
-        noImplicitReturns: false,
-        typeRoots: [
-          'types',
-          'node_modules/@polkadot/ts/types/',
-          'node_modules/@types',
-        ],
-        esModuleInterop: false,
-        inlineSourceMap: false,
-        inlineSources: false,
-      },
+      tsConfig: '<rootDir>/test.tsconfig.json',
     },
   },
 
