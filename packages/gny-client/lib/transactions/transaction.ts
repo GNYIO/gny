831a64294a206cd7e4715208869311e5bf3d7714
xpgeng <xpgeng.me@gmail.com>
2019-10-12 23:07:54 +0800

    chore: delete unused property

--- packages/gny-client/lib/transactions/transaction.ts
+++ packages/gny-client/lib/transactions/transaction.ts
@@ -4,12 +4,6 @@ import slots = require('../time/slots');
 import options = require('../options');
 import { ITransaction } from '@gny/interfaces';
 
-export function calculateFee(amount: number) {
-  const min = constants.fees.send;
-  const fee = parseFloat((amount * 0.0001).toFixed(0));
-  return fee < min ? min : fee;
-}
-
 export function createTransactionEx(params: any) {
   if (!params.secret) throw new Error('Secret needed');
   const keys = crypto.getKeys(params.secret);
