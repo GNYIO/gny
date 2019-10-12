7cdc51be1555e43d26915b6fd872b0e4fb3290f9
xpgeng <xpgeng.me@gmail.com>
2019-10-13 00:19:13 +0800

    chore: modify interface

--- packages/gny-client/lib/transactions/transaction.ts
+++ packages/gny-client/lib/transactions/transaction.ts
@@ -1,11 +1,12 @@
-import crypto = require('./crypto');
+// import crypto = require('./crypto');
+import * as crypto from './crypto';
 import { slots } from '@gny/utils';
 import { ITransaction } from '@gny/interfaces';
 
 interface Params {
   type: number;
   fee: number;
-  args: string[];
+  args: (string | number)[];
   secret: string;
   message?: string;
   secondSecret?: string;
@@ -13,7 +14,7 @@ interface Params {
 export function createTransactionEx(params: Params) {
   if (!params.secret) throw new Error('Secret needed');
   const keys = crypto.getKeys(params.secret);
-  const transaction: any = {
+  const transaction = {
     type: params.type,
     timestamp: slots.getTime(),
     fee: String(params.fee),
@@ -22,7 +23,7 @@ export function createTransactionEx(params: Params) {
     senderPublicKey: keys.publicKey,
     senderId: crypto.getAddress(keys.publicKey),
     signatures: [],
-  };
+  } as ITransaction;
   transaction.signatures.push(crypto.sign(transaction, keys));
   if (params.secondSecret) {
     const secondKeys = crypto.getKeys(params.secondSecret);
