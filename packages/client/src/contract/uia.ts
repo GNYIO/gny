1aafd9a51e0fd77661330703182f7ff6938294e9
xpgeng <xpgeng.me@gmail.com>
2019-10-30 19:44:25 +0800

    chore: move uia contract to new folder

--- packages/client/src/contract/uia.ts
+++ packages/client/src/contract/uia.ts
@@ -1,4 +1,39 @@
 import { Base } from '../api/base';
 import { uia } from '../';
 
-export class Uia extends Base {}
+export class Uia extends Base {
+  public async registerIssuer(
+    name: string,
+    desc: string,
+    secret: string,
+    secondSecret?: string
+  ) {
+    const trs = uia.registerIssuer(name, desc, secret, secondSecret);
+    const params = {
+      transaction: trs,
+    };
+    return await this.post('/peer/transactions', params);
+  }
+
+  public async registerAsset(
+    name: string,
+    desc: string,
+    maximum: string,
+    precision: number,
+    secret: string,
+    secondSecret?: string
+  ) {
+    const trs = uia.registerAsset(
+      name,
+      desc,
+      maximum,
+      precision,
+      secret,
+      secondSecret
+    );
+    const params = {
+      transaction: trs,
+    };
+    return await this.post('/peer/transactions', params);
+  }
+}
