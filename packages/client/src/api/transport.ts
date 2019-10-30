15ccd9e8abb88d8a74eace776c594fc09b5e209b
xpgeng <xpgeng.me@gmail.com>
2019-10-30 16:31:53 +0800

    chore: add more APIs for transport

--- packages/client/src/api/transport.ts
+++ packages/client/src/api/transport.ts
@@ -1,11 +1,60 @@
 import { Base } from './base';
 import { ITransaction } from '@gny/interfaces';
 
+interface Keypair {
+  publicKey: string;
+  signature: string;
+}
+interface Votes {
+  height: string;
+  id: string;
+  signatures: Keypair[];
+}
+
 export class Transport extends Base {
   public async sendTransaction(transaction: ITransaction) {
     const params = {
       transaction: transaction,
     };
     return await this.post('/peer/transactions', params);
+  }
+
+  public async getLatestLock(id: string) {
+    const params = {
+      id: id,
+    };
+    return await this.post('/peer/newBlock', params);
+  }
+
+  public async getBlocksByIds(max: string, min: string, ids: string[]) {
+    const params = {
+      max,
+      min,
+      ids,
+    };
+    return await this.post('/peer/commonBlock', params);
+  }
+
+  public async getBlocksByLimit(limit: number, lastBlockId: string) {
+    const params = {
+      limit,
+      lastBlockId,
+    };
+    return await this.post('/peer/blocks', params);
+  }
+
+  public async validateVote(votes: Votes) {
+    const params = {
+      votes: votes,
+    };
+    return await this.post('/peer/votes', params);
+  }
+
+  public async getUnconfirmedTransactions() {
+    return await this.post('/peer/getUnconfirmedTransactions');
+  }
+
+  public async getHeight() {
+    return await this.post('/peer/getHeight');
   }
 }
