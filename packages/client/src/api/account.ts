26a584e4e9c45a5ca1ae3ab021dc4eec6468060c
xpgeng <xpgeng.me@gmail.com>
2019-11-06 17:06:07 +0800

    chore: integrate the new types into accountApi with type casting #182

--- packages/client/src/api/account.ts
+++ packages/client/src/api/account.ts
@@ -1,4 +1,15 @@
 import { Base } from './base';
+import {
+  DelegateViewModel,
+  IBalance,
+  ApiResponse,
+  AccountGenerateModel,
+  IAccount,
+  ServerError,
+  AccountOpenModel,
+  ResponseError,
+  BalanceResponseError,
+} from '@gny/interfaces';
 
 interface OnlyAddress {
   address: string;
@@ -8,50 +19,89 @@ interface OnlyUserName {
   username: string;
 }
 
+interface BalanceResult {
+  count: number;
+  balances: IBalance[];
+}
+
+interface DelegateResult {
+  delegates: DelegateViewModel[];
+}
+
+interface CountResult {
+  count: number;
+}
+
+type PublicKeyError = 'Can not find public key';
+
 export class Account extends Base {
-  public async generateAccount() {
+  public async generateAccount<
+    T = AccountGenerateModel,
+    K = ServerError
+  >(): Promise<ApiResponse<T, K>> {
     return await this.get('/api/accounts/generateAccount');
   }
 
-  public async openAccount(secret: string) {
+  public async openAccount<T = AccountOpenModel, K = ResponseError>(
+    secret: string
+  ): Promise<ApiResponse<T, K>> {
     return await this.post('/api/accounts/open', { secret: secret });
   }
 
-  public async getBalance(address: string) {
+  public async getBalance<T = BalanceResult, K = ResponseError>(
+    address: string
+  ): Promise<ApiResponse<T, K>> {
     const params = { address: address };
     return await this.get('/api/accounts/getBalance', params);
   }
 
-  public async getAddressCurrencyBalance(address: string, currency: string) {
+  public async getAddressCurrencyBalance<
+    T = IBalance,
+    K = BalanceResponseError
+  >(address: string, currency: string): Promise<ApiResponse<T, K>> {
     return await this.get(`/api/accounts/${address}/${currency}`);
   }
 
-  public async getAccountByAddress(address: string) {
+  public async getAccountByAddress<T = AccountOpenModel, K = ResponseError>(
+    address: string
+  ): Promise<ApiResponse<T, K>> {
     const params = { address: address };
     return await this.get('/api/accounts/', params);
   }
 
-  public async getAccountByUsername(username: string) {
+  public async getAccountByUsername<T = IAccount, K = ResponseError>(
+    username: string
+  ): Promise<ApiResponse<T, K>> {
     const params = { username: username };
     return await this.get('/api/accounts/', params);
   }
 
-  public async getVotedDelegates(query: OnlyAddress | OnlyUserName) {
+  public async getVotedDelegates<T = DelegateResult, K = ResponseError>(
+    query: OnlyAddress | OnlyUserName
+  ): Promise<ApiResponse<T, K>> {
     return await this.get('/api/accounts/getVotes', query);
   }
 
-  public async countAccounts() {
+  public async countAccounts<T = CountResult, K = ServerError>(): Promise<
+    ApiResponse<T, K>
+  > {
     return await this.get('/api/accounts/count');
   }
 
-  public async getPublicKey(address: string) {
+  public async getPublicKey<
+    T = Pick<IAccount, 'publicKey'>,
+    K = ResponseError | PublicKeyError
+  >(address: string): Promise<ApiResponse<T, K>> {
     const params = {
       address: address,
     };
     return await this.get('/api/accounts/getPublicKey', params);
   }
 
-  public async generatePublicKey(secret: string) {
+  public async generatePublicKey<
+    T = Pick<IAccount, 'publicKey'>,
+    K = ResponseError
+  >(secret: string): Promise<ApiResponse<T, K>> {
     const params = {
       secret: secret,
     };
