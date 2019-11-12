3a630145fb4510a79930238366d7f6deb9616467
xpgeng <xpgeng.me@gmail.com>
2019-11-12 20:50:03 +0800

    chore: add return type assertion for client delegate api #182

--- packages/client/src/api/delegate.ts
+++ packages/client/src/api/delegate.ts
@@ -1,36 +1,67 @@
 import { Base } from './base';
-
+import {
+  ApiResult,
+  CountWrapper,
+  DelegateResponseError,
+  AccountsWrapper,
+  ValidationError,
+  ServerError,
+  DelegateWrapper,
+  DelegatesWrapper,
+  ForgingError,
+  ForgingStatus,
+} from '@gny/interfaces';
 export class Delegate extends Base {
   public async count() {
-    return await this.get('/api/delegates/count');
+    const res = await this.get('/api/delegates/count');
+    const result: ApiResult<CountWrapper, DelegateResponseError> = res.data;
+    return result;
   }
 
   public async getVoters(username: string) {
     const params = {
       username: username,
     };
-    return await this.get('/api/delegates/getVoters', params);
+    const res = await this.get('/api/delegates/getVoters', params);
+    const result: ApiResult<AccountsWrapper, ValidationError | ServerError> =
+      res.data;
+    return result;
   }
 
   public async getDelegateByPubKey(publicKey: string) {
     const params = {
       publicKey: publicKey,
     };
-    return await this.get('/api/delegates/get', params);
+    const res = await this.get('/api/delegates/get', params);
+    const result: ApiResult<
+      DelegateWrapper,
+      ValidationError | DelegateResponseError
+    > = res.data;
+    return result;
   }
 
   public async getDelegateByUsername(username: string) {
     const params = {
       username: username,
     };
-    return await this.get('/api/delegates/get', params);
+    const res = await this.get('/api/delegates/get', params);
+    const result: ApiResult<
+      DelegateWrapper,
+      ValidationError | DelegateResponseError
+    > = res.data;
+    return result;
   }
 
   public async getDelegateByAddress(address: string) {
     const params = {
       address: address,
     };
-    return await this.get('/api/delegates/get', params);
+    const res = await this.get('/api/delegates/get', params);
+    const result: ApiResult<
+      DelegateWrapper,
+      ValidationError | DelegateResponseError
+    > = res.data;
+    return result;
   }
 
   public async getDelegates(offset?: string, limit?: string) {
@@ -38,7 +69,12 @@ export class Delegate extends Base {
       offset: offset,
       limit: limit,
     };
-    return await this.get('/api/delegates', params);
+    const res = await this.get('/api/delegates', params);
+    const result: ApiResult<
+      DelegatesWrapper,
+      ValidationError | DelegateResponseError
+    > = res.data;
+    return result;
   }
 
   public async forgingEnable(secret: string, pulicKey: string) {
@@ -46,7 +82,9 @@ export class Delegate extends Base {
       secret: secret,
       pulicKey: pulicKey,
     };
-    return await this.get('/api/delegates/forging/enable', params);
+    const res = await this.get('/api/delegates/forging/enable', params);
+    const result: ApiResult<any, ValidationError | ForgingError> = res.data;
+    return result;
   }
 
   public async forgingDisable(secret: string, pulicKey: string) {
@@ -54,13 +92,18 @@ export class Delegate extends Base {
       secret: secret,
       pulicKey: pulicKey,
     };
-    return await this.get('/api/delegates/forging/disable', params);
+    const res = await this.get('/api/delegates/forging/disable', params);
+    const result: ApiResult<any, ValidationError | ForgingError> = res.data;
+    return result;
   }
 
   public async forgingStatus(publicKey: string) {
     const params = {
       publicKey: publicKey,
     };
-    return await this.get('/api/delegates/forging/status', params);
+    const res = await this.get('/api/delegates/forging/status', params);
+    const result: ApiResult<ForgingStatus, ValidationError | ForgingError> =
+      res.data;
+    return result;
   }
 }
