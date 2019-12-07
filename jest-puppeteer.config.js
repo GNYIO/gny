2b96f4aae4a1e6a7137708b8c978cc9f43c86ae9
xpgeng <xpgeng.me@gmail.com>
2019-12-07 18:56:16 +0800

    chore: add config file for browser tests

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  server: {
+    command: 'npm run test:server',
+    port: 4444,
+    launchTimeout: 30000,
+    debug: true,
+  },
+};
---
+++ jest.config.client.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'jest-puppeteer',
+  roots: ['<rootDir>/test/unit/client/browser/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: false,
+  globals: {
+    PATH: 'http://localhost:4444/',
+    TIMEOUT: 5000,
+    'ts-jest': {
+      diagnostics: false,
+      tsConfig: '<rootDir>/jest.tsconfig.json',
+    },
+  },
+
+  moduleNameMapper: {
+    '@gny/(.*)$': '<rootDir>/packages/$1',
+  },
+};
