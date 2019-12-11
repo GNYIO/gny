f88a064b8b5a8d0d9a32ac813da24b41e5e4a4ba
xpgeng <xpgeng.me@gmail.com>
2019-12-11 17:41:07 +0800

    chore: move the functionality of PUT /api/transactions to PUT /api/exchange #205

--- packages/main/src/http/api/transactionsApi.ts
+++ packages/main/src/http/api/transactionsApi.ts
@@ -4,7 +4,6 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 import {
   IScope,
-  KeyPair,
   Next,
   ITransaction,
   IBlock,
@@ -13,11 +12,8 @@ import {
   ApiResult,
   TransactionsWrapper,
   UnconfirmedTransactionWrapper,
-  TransactionIdWrapper,
 } from '@gny/interfaces';
-import { TransactionBase } from '@gny/base';
 import { StateHelper } from '../../../src/core/StateHelper';
-import Transactions from '../../../src/core/transactions';
 import { Transaction } from '@gny/database-postgres';
 import { joi } from '@gny/extendedJoi';
 
@@ -42,8 +38,6 @@ export default class TransactionsApi implements IHttpApi {
     router.get('/', this.getTransactions);
     router.get('/unconfirmed/get', this.getUnconfirmedTransaction);
     router.get('/unconfirmed', this.getUnconfirmedTransactions);
-    router.put('/', this.addTransactionUnsigned);
-    router.put('/batch', this.addTransactions);
 
     router.use((req: Request, res: Response) => {
       res.status(500).json({ success: false, error: 'API endpoint not found' });
@@ -236,144 +230,5 @@ export default class TransactionsApi implements IHttpApi {
       transactions: toSend,
     };
     return res.json(result);
-  };
-
-  private addTransactionUnsigned = (
-    req: Request,
-    res: Response,
-    next: Next
-  ) => {
-    const query = req.body;
-    const unsigendTransactionSchema = joi.object().keys({
-      secret: joi
-        .string()
-        .secret()
-        .required(),
-      secondSecret: joi
-        .string()
-        .secret()
-        .optional(),
-      fee: joi
-        .string()
-        .positiveOrZeroBigInt()
-        .required(),
-      type: joi
-        .number()
-        .min(0)
-        .required(),
-      args: joi.array().optional(),
-      message: joi
-        .string()
-        .max(256)
-        .alphanum()
-        .allow('')
-        .optional(),
-      senderId: joi
-        .string()
-        .address()
-        .optional(),
-    });
-    const report = joi.validate(query, unsigendTransactionSchema);
-    if (report.error) {
-      this.library.logger.warn(
-        'Failed to validate query params',
-        report.error.message
-      );
-      return setImmediate(next, 'Invalid transaction body');
-    }
-
-    const finishSequence = (err: string, result: any) => {
-      if (err) {
-        return next(err);
-      }
-      res.json(result);
-    };
-
-    this.library.sequence.add(
-      callback => {
-        (async () => {
-          const state = StateHelper.getState();
-
-          try {
-            const hash = crypto
-              .createHash('sha256')
-              .update(query.secret, 'utf8')
-              .digest();
-            const keypair = ed.generateKeyPair(hash);
-            let secondKeypair: KeyPair = null;
-            if (query.secondSecret) {
-              secondKeypair = ed.generateKeyPair(
-                crypto
-                  .createHash('sha256')
-                  .update(query.secondSecret, 'utf8')
-                  .digest()
-              );
-            }
-            const unconfirmedTrs = TransactionBase.create({
-              fee: query.fee,
-              type: query.type,
-              args: query.args || null,
-              message: query.message || null,
-              secondKeypair,
-              keypair,
-            });
-            await Transactions.processUnconfirmedTransactionAsync(
-              state,
-              unconfirmedTrs
-            );
-            this.library.bus.message(
-              'onUnconfirmedTransaction',
-              unconfirmedTrs
-            );
-            const result: ApiResult<TransactionIdWrapper> = {
-              success: true,
-              transactionId: unconfirmedTrs.id,
-            };
-            callback(null, result);
-          } catch (e) {
-            this.library.logger.warn(
-              'Failed to process unsigned transaction',
-              e
-            );
-            callback('Server Error');
-          }
-        })();
-      },
-      undefined,
-      finishSequence
-    );
-  };
-
-  private addTransactions = (req: Request, res: Response, next: Next) => {
-    if (!req.body || !req.body.transactions) {
-      return next('Invalid params');
-    }
-    const finishedCallback = (err: string, result: any) => {
-      if (err) {
-        return next(err);
-      }
-      const trsResult: ApiResult<TransactionsWrapper> = {
-        success: true,
-        transactions: result,
-      };
-      return res.json(trsResult);
-    };
-
-    const trs = req.body.transactions;
-    try {
-      for (let i = 0; i < trs.length; ++i) {
-        trs[i] = TransactionBase.normalizeTransaction(trs[i]);
-      }
-    } catch (e) {
-      return next(`Invalid transaction body: ${e.toString()}`);
-    }
-    return this.library.sequence.add(
-      callback => {
-        const state = StateHelper.getState();
-        Transactions.processUnconfirmedTransactions(state, trs, callback);
-      },
-      undefined,
-      finishedCallback
-    );
   };
 }
