3df8f7efb2d2cc4ea8fd3b2b60bd9ab808071acb
xpgeng <xpgeng.me@gmail.com>
2020-01-13 23:36:31 +0800

    chore: add new interfaces about cli #140

--- packages/interfaces/src/index.ts
+++ packages/interfaces/src/index.ts
@@ -209,6 +209,12 @@ export interface SimplePeerInfo {
   simple: PeerNode;
 }
 
+export interface PeerStateWrapper {
+  peer: SimplePeerInfo;
+  height: string;
+  error?: string;
+}
+
 export interface ProcessBlockOptions {
   local?: true;
   broadcast?: true;
@@ -577,6 +583,11 @@ export interface DelegateWrapper {
 export interface DelegatesWrapper {
   totalCount?: number;
   delegates: DelegateViewModel[];
+}
+
+export interface DelegateStateWrapper {
+  delegate: DelegateViewModel;
+  block: IBlock;
 }
 
 export interface ForgingStatus {
