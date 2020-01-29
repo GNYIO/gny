5e3e36ec56220a79c390d87bb43ec4e9a4f0627a
Matthias Knopp <matthias-knopp@gmx.net>
2020-01-29 12:30:27 +0100

    feat: make less than 101 delegates, #250

--- packages/cli/src/lib/newGenesisBlock.ts
+++ packages/cli/src/lib/newGenesisBlock.ts
@@ -14,6 +14,13 @@ import {
 import * as addressHelper from '@gny/utils';
 import { AccountType } from './account';
 
+interface DelegateAccount {
+  keypair: KeyPair;
+  address: string;
+  secret: string;
+  username: string;
+}
+
 type CreateTransactionTypeWithTimestamp = CreateTransactionType & {
   timestamp: number;
 };
@@ -59,9 +66,25 @@ export default function newGenesisBlock(program: any) {
   program
     .command('creategenesis')
     .description('create genesis block')
-    .option('-f, --file <file>', 'genesis accounts balance file')
-    .option('-d, --delegates <file>', 'BIP39 secrets of 101 delegates')
-    .option('-g, --genesis <account>', 'BIP39 complient secret')
+    .option(
+      '-f, --file <file>',
+      `genesis accounts balance file. Format:
+                            <address><tab><amount><newline>
+                            <address><tab>amount><newline>
+      `
+    )
+    .option(
+      '-d, --delegates <file>',
+      `BIP39 secrets of 101 delegates. Format:
+                            <delegate_name><tab><BIP39_secret><newline>
+                            <delegate_name><tab><BIP39_secret><newline>
+                            <delegate_name><tab><BIP39_secret><newline>
+      `
+    )
+    .option(
+      '-g, --genesis <account>',
+      'BIP39 complient secret. Default is a random genesis account'
+    )
     .option(
       '-a, --amount <number>',
       'Initial amount for blockchain; must be have precision of 8'
@@ -80,9 +103,9 @@ function appendFileSync(file, obj) {
 }
 
 function genGenesisBlock(options) {
-  let initialAmount = 100000000 * 1e8;
+  let initialAmount = String(100000000 * 1e8);
   if (options.amount) {
-    initialAmount = Number(options.amount);
+    initialAmount = String(options.amount);
   }
 
   let genesisAccount: AccountType;
@@ -94,6 +117,7 @@ function genGenesisBlock(options) {
   const newBlockInfo = generateGenesis(
     genesisAccount,
     options.file,
+    options.delegates,
     initialAmount
   );
   const delegateSecrets = newBlockInfo.delegates.map(i => i.secret);
@@ -121,12 +145,12 @@ const sender = accountHelper.account(generateSecret());
 function generateGenesis(
   genesisAccount: AccountType,
   accountsFile: string,
-  intialAmount
+  delegatesFile: string,
+  intialAmount: string
 ) {
   let payloadLength = 0;
   const transactions: UnconfirmedTransaction[] = [];
   const payloadHash = crypto.createHash('sha256');
-  const delegates = [];
 
   // fund recipient account
   if (accountsFile && fs.existsSync(accountsFile)) {
@@ -160,38 +184,24 @@ function generateGenesis(
     transactions.push(createTransaction(balanceTransaction));
   }
 
-  // make delegates
-  for (let i = 0; i < 101; i++) {
-    const delegate = accountHelper.account(generateSecret());
+  const delegates: DelegateAccount[] = createDelegates(delegatesFile);
 
-    const username = 'gny_d' + (i + 1);
-    interface DelegateAccount {
-      keypair: KeyPair;
-      address: string;
-      secret: any;
-      name: string;
-    }
-
-    // delegate.name = username;
-    const finishedDelegate: DelegateAccount = {
-      ...delegate,
-      name: username,
-    };
-    delegates.push(finishedDelegate);
+  for (let i = 0; i < delegates.length; i++) {
+    const one = delegates[i];
 
     const nameTrs: CreateTransactionTypeWithTimestamp = {
       type: 1,
       timestamp: 0,
       fee: String(0),
-      args: [username],
-      keypair: finishedDelegate.keypair,
+      args: [one.username],
+      keypair: one.keypair,
     };
     const delegateTrs: CreateTransactionTypeWithTimestamp = {
       type: 10,
       timestamp: 0,
       args: [],
       fee: String(0),
-      keypair: finishedDelegate.keypair,
+      keypair: one.keypair,
     };
 
     transactions.push(createTransaction(nameTrs));
@@ -241,4 +251,48 @@ function generateGenesis(
     block: finalBlock,
     delegates: delegates,
   };
+}
+
+function createDelegates(delegatesFile?: string) {
+  const delegates: DelegateAccount[] = [];
+
+  if (!delegatesFile) {
+    for (let i = 0; ++i; i < 101) {
+      const delegate = accountHelper.account(generateSecret());
+
+      const username = 'gny_d' + (i + 1);
+
+      // delegate.name = username;
+      const finishedDelegate: DelegateAccount = {
+        ...delegate,
+        username: username,
+      };
+      delegates.push(finishedDelegate);
+    }
+  }
+
+  if (delegatesFile) {
+    const lines = fs.readFileSync(delegatesFile, 'utf8').split('\n');
+    console.log(`lines: ${JSON.stringify(lines, null, 2)}`);
+    for (const i in lines) {
+      const parts = lines[i].split('\t');
+      if (parts.length != 2) {
+        console.log(JSON.stringify(parts, null, 2));
+        console.error('Invalid delegates file format');
+        process.exit(1);
+      }
+
+      const username = parts[0];
+      const secret = parts[1];
+
+      const delegate = accountHelper.account(secret);
+      const finishedDelegate: DelegateAccount = {
+        ...delegate,
+        username: username,
+      };
+      delegates.push(finishedDelegate);
+    }
+  }
+
+  return delegates;
 }
