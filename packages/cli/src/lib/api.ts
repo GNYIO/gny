0cfa30164f2f8953c6bab1db19fc600330a26b00
xpgeng <xpgeng.me@gmail.com>
2020-02-10 22:16:21 +0800

    chore: modify api helper function

--- packages/cli/src/lib/api.ts
+++ packages/cli/src/lib/api.ts
@@ -8,6 +8,12 @@ const config = {
   },
 };
 
+function pretty(obj: any) {
+  return JSON.stringify(obj, null, 2);
+}
+
+export const http = axios.create();
+
 export interface ApiOptions {
   host: string;
   port: number;
@@ -15,61 +21,27 @@ export interface ApiOptions {
 
 export type ApiConfig = Partial<program.CommanderStatic> & ApiOptions;
 
-export class Api {
-  options: ApiConfig;
-  host: string;
-  port: number;
-  baseUrl: string;
-  magic: string;
-
-  constructor(options: ApiConfig) {
-    this.options = options;
-    this.host = this.options.host || '127.0.0.1';
-    this.port = this.options.port || 4096;
-    this.baseUrl = `http://${this.host}:${this.port}`;
-    this.magic = '594fe0f3';
+export async function get(url, params?) {
+  try {
+    const { data } = await http.get(url, {
+      params: params,
+    });
+    console.log(pretty(data));
+  } catch (error) {
+    console.log(error.response.data);
   }
-
-  get = function(path: string, params, cb?) {
-    let qs = null;
-    if (typeof params === 'function') {
-      cb = params;
-    } else {
-      qs = params;
-    }
-
-    axios
-      .get(this.baseUrl + path, { params: qs })
-      .then(function(response) {
-        cb(null, response.data);
-      })
-      .catch(function(error) {
-        cb(error, null);
-      });
-  };
-
-  post = function(path, data, cb) {
-    axios
-      .post(this.baseUrl + path, data, config)
-      .then(function(response) {
-        cb(null, response.data);
-      })
-      .catch(function(error) {
-        cb(error, null);
-      });
-  };
-
-  broadcastTransaction = function(trs: UnconfirmedTransaction, cb) {
-    const data = {
-      transaction: trs,
-    };
-    axios
-      .post(this.baseUrl + '/peer/transactions', data, config)
-      .then(function(response) {
-        cb(null, response.data);
-      })
-      .catch(function(error) {
-        cb(error, null);
-      });
-  };
 }
+
+export async function post(url, params) {
+  try {
+    const { data } = await http.post(url, params, config);
+    console.log(pretty(data));
+  } catch (error) {
+    console.log(error.response.data);
+  }
+}
+
+export default {
+  get: get,
+  post: post,
+};
