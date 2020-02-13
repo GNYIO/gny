bef34166b9f12388a491b40bbd4404c74298de8a
xpgeng <xpgeng.me@gmail.com>
2020-02-13 17:19:06 +0800

    chore: add default url for unit tests

--- packages/cli/src/getBaseUrl.ts
+++ packages/cli/src/getBaseUrl.ts
@@ -1,3 +1,6 @@
 export function getBaseUrl() {
-  return `http://${global.host}:${global.port}`;
+  const host = global.host || '127.0.0.1';
+  const port = global.port || 4096;
+
+  return `http://${host}:${port}`;
 }
