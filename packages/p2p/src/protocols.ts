c63fa1de161d0ac3d6cab48b80b74498df1be995
Matthias Knopp <matthias-knopp@gmx.net>
2020-10-26 21:40:09 +0100

    fix: typo in p2p blocks protocol [ci skip]

--- packages/p2p/src/protocols.ts
+++ packages/p2p/src/protocols.ts
@@ -2,7 +2,7 @@ export const V1_NEW_BLOCK_PROTOCOL = '/v1/newBlock';
 export const V1_VOTES = '/v1/votes';
 export const V1_COMMON_BLOCK = '/v1/commonBlock';
 export const V1_GET_HEIGHT = '/v1/getHeight';
-export const V1_BLOCK = '/v1/blocks';
+export const V1_BLOCKS = '/v1/blocks';
 
 export const V1_BROADCAST_NEW_BLOCK_HEADER = '/v1/broadcast/newBlockHeader';
 export const V1_BROADCAST_TRANSACTION = '/v1/broadcast/transaction';
