3f79b38599208e88fe8ce3047b85ce54f34bf0dd
Matthias Knopp <matthias-knopp@gmx.net>
2020-11-13 14:36:38 +0100

    chore: remove vlid package  #367

--- packages/client/src/connection.ts
+++ packages/client/src/connection.ts
@@ -1,25 +1,29 @@
 import { Api } from './api';
 import { Contract } from './contract';
 import { NetworkType } from '@gny/interfaces';
-const v = require('vlid');
 
-function isIpOrUrl(value: any) {
+function isIpOrUrl(value: any): value is string {
   const ip = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   const url = /^([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/;
 
   return ip.test(value) || url.test(value);
 }
 
-function isPort(value: any) {
+function isPort(value: any): value is number {
   const result = Number.isInteger(value) && value > 0 && value < 65535;
   return result;
 }
 
-function isNetworkType(value: string) {
+function isNetworkType(value: any): value is NetworkType {
   if (value === 'localnet' || value === 'testnet' || value === 'mainnet') {
     return true;
   }
   return false;
+}
+
+function isBoolean(value: any): value is boolean {
+  const result = typeof value === 'boolean';
+  return result;
 }
 
 export class Connection {
@@ -43,32 +47,20 @@ export class Connection {
       this.baseUrl = `${https ? 'https' : 'http'}://${this.host}:${this.port}`;
     }
 
-    const config = {
-      host: this.host,
-      port: this.port,
-      network: this.network,
-      https: https,
-    };
-    const schema = v
-      .object({
-        host: v
-          .string()
-          .rule(isIpOrUrl)
-          .required(),
-        port: v
-          .number()
-          .rule(isPort)
-          .required(),
-        network: v
-          .string()
-          .rule(isNetworkType)
-          .required(),
-        https: v.boolean().required(),
-      })
-      .required();
-    const report = v.validateSync(schema, config);
-    if (report.isValid === false) {
-      throw new Error(report.errors[0].message);
+    if (!isIpOrUrl(this.host)) {
+      throw new Error('host not valid');
+    }
+
+    if (!isPort(this.port)) {
+      throw new Error('port not valid');
+    }
+
+    if (!isNetworkType(this.network)) {
+      throw new Error('networktype not valid');
+    }
+
+    if (!isBoolean(https)) {
+      throw new Error('https not valid');
     }
   }
 
