80960d3eda07e19e344fef49c3278f6069eb5d31
Matthias Knopp <matthias-knopp@gmx.net>
2020-11-13 08:35:35 +0100

    fix: check @gny/client con constructor #367

--- packages/client/src/connection.ts
+++ packages/client/src/connection.ts
@@ -1,6 +1,26 @@
 import { Api } from './api';
 import { Contract } from './contract';
 import { NetworkType } from '@gny/interfaces';
+const v = require('vlid');
+
+function isIpOrUrl(value: any) {
+  const ip = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  const url = /^([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/;
+
+  return ip.test(value) || url.test(value);
+}
+
+function isPort(value: any) {
+  const result = Number.isInteger(value) && value > 0 && value < 65535;
+  return result;
+}
+
+function isNetworkType(value: string) {
+  if (value === 'localnet' || value === 'testnet' || value === 'mainnet') {
+    return true;
+  }
+  return false;
+}
 
 export class Connection {
   host: string;
@@ -22,7 +42,34 @@ export class Connection {
     } else {
       this.baseUrl = `${https ? 'https' : 'http'}://${this.host}:${this.port}`;
     }
-    console.log(`@gny/client baseUrl ${this.baseUrl}`);
+
+    const config = {
+      host: this.host,
+      port: this.port,
+      network: this.network,
+      https: https,
+    };
+    const schema = v
+      .object({
+        host: v
+          .string()
+          .rule(isIpOrUrl)
+          .required(),
+        port: v
+          .number()
+          .rule(isPort)
+          .required(),
+        network: v
+          .string()
+          .rule(isNetworkType)
+          .required(),
+        https: v.boolean().required(),
+      })
+      .required();
+    const report = v.validateSync(schema, config);
+    if (report.isValid === false) {
+      throw new Error(report.errors[0].message);
+    }
   }
 
   public api = Api(this);
--- test/unit/client/index.test.ts
+++ test/unit/client/index.test.ts
@@ -31,4 +31,119 @@ describe('GNY client', () => {
       expect(address).toEqual('G3ENMYpn7kTwjyai49kJezjkJvaQk');
     });
   });
+
+  describe('test connection constructor', () => {
+    describe('url', () => {
+      it('should throw when http is used in url', () => {
+        const url = 'http://test.com';
+        expect(() => new gnyClient.Connection(url)).toThrow();
+      });
+
+      it('should throw when https is used in url', () => {
+        const url = 'https://test.com';
+        expect(() => new gnyClient.Connection(url)).toThrow();
+      });
+
+      it('should pass when naked url url is used', () => {
+        const url = 'test.com';
+        expect(() => new gnyClient.Connection(url)).not.toThrow();
+      });
+
+      it('should pass when naked url with subdomain is used', () => {
+        const url = 'testnet.test.com';
+        expect(() => new gnyClient.Connection(url)).not.toThrow();
+      });
+
+      it('should pass when ip for url is used', () => {
+        const url = '127.0.0.1';
+        expect(() => new gnyClient.Connection(url)).not.toThrow();
+      });
+    });
+
+    describe('port', () => {
+      it('port -1 not valid', () => {
+        const url = '127.0.0.1';
+        const port = -1;
+        expect(() => new gnyClient.Connection(url, port)).toThrow();
+      });
+
+      it('port 4096 is valid', () => {
+        const url = '127.0.0.1';
+        const port = 4096;
+        expect(() => new gnyClient.Connection(url, port)).not.toThrow();
+      });
+
+      it('port 80000 not valid', () => {
+        const url = '127.0.0.1';
+        const port = 80000;
+        expect(() => new gnyClient.Connection(url, port)).toThrow();
+      });
+
+      it('port 2444.2 not valid', () => {
+        const url = '127.0.0.1';
+        const port = 2444.2;
+        expect(() => new gnyClient.Connection(url, port)).toThrow();
+      });
+    });
+
+    describe('network', () => {
+      it('localnet is valid', () => {
+        const url = '127.0.0.1';
+        const port = 4096;
+        const net = 'localnet';
+        expect(() => new gnyClient.Connection(url, port, net)).not.toThrow();
+      });
+
+      it('testnet is valid', () => {
+        const url = '127.0.0.1';
+        const port = 4096;
+        const net = 'testnet';
+        expect(() => new gnyClient.Connection(url, port, net)).not.toThrow();
+      });
+
+      it('mainnet is valid', () => {
+        const url = '127.0.0.1';
+        const port = 4096;
+        const net = 'mainnet';
+        expect(() => new gnyClient.Connection(url, port, net)).not.toThrow();
+      });
+
+      it('hello is not valid', () => {
+        const url = '127.0.0.1';
+        const port = 4096;
+        const net = 'hello';
+        expect(() => new gnyClient.Connection(url, port, net)).toThrow();
+      });
+    });
+
+    describe('https', () => {
+      it('true is valid', () => {
+        const url = 'test.com';
+        const port = 4096;
+        const net = 'localnet';
+        const https = true;
+        expect(
+          () => new gnyClient.Connection(url, port, net, https)
+        ).not.toThrow();
+      });
+
+      it('false is valid', () => {
+        const url = 'test.com';
+        const port = 4096;
+        const net = 'localnet';
+        const https = false;
+        expect(
+          () => new gnyClient.Connection(url, port, net, https)
+        ).not.toThrow();
+      });
+
+      it('2 is invalid', () => {
+        const url = 'test.com';
+        const port = 4096;
+        const net = 'localnet';
+        const https = 2;
+        expect(() => new gnyClient.Connection(url, port, net, https)).toThrow();
+      });
+    });
+  });
 });
