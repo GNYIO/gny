082752e84b6f667c881ed39487552785dc2ab578
Matthias Knopp <matthias-knopp@gmx.net>
2022-06-26 12:21:10 +0200

    e2e: fix e2e test unconfirmed-trs.test

--- test/e2e/unconfirmed-trs.test.ts
+++ test/e2e/unconfirmed-trs.test.ts
@@ -5,6 +5,7 @@ import * as gnyClient from '@gny/client';
 import axios from 'axios';
 import * as _ from 'lodash';
 import { getConfig } from '@gny/network';
+import { UnconfirmedTransaction } from '@gny/interfaces';
 
 const DOCKER_COMPOSE_P2P =
   'config/e2e/unconfirmed-trs/docker-compose.unconfirmed-trs.yml';
@@ -24,12 +25,42 @@ async function getUnconfirmedTrsCount(port: number) {
   return count;
 }
 
+async function getUnconfirmedTransactions(port: number) {
+  let transactions: UnconfirmedTransaction[] = [];
+  try {
+    const { data } = await axios.get(
+      `http://localhost:${port}/api/transactions/unconfirmed`
+    );
+    transactions = data.transactions as UnconfirmedTransaction[];
+  } catch (err) {}
+  return transactions;
+}
+
+let continueRequest = true;
+
+async function constantQuery(
+  port: number,
+  map: Map<string, UnconfirmedTransaction>
+) {
+  while (continueRequest) {
+    const transactions = await getUnconfirmedTransactions(port);
+    for (let i = 0; i < transactions.length; ++i) {
+      const one = transactions[i];
+      const trsId = one.id;
+      if (!map.has(trsId)) {
+        map.set(trsId, one);
+      }
+    }
+    await lib.sleep(250);
+  }
+}
+
 function createTransactions(count: number) {
   const genesisSecret = getConfig('localnet').genesis;
   const message = '';
   const amount = 5 * 1e8;
 
-  const transactions = [];
+  const transactions: UnconfirmedTransaction[] = [];
 
   for (let i = 0; i < count; ++i) {
     const recipient = lib.createRandomAddress();
@@ -43,16 +74,22 @@ function createTransactions(count: number) {
   }
   return transactions;
 }
+
 async function sendRandomTransaction(numberOfTransaction: number) {
   const transactions = createTransactions(numberOfTransaction);
   for (let i = 0; i < transactions.length; ++i) {
     const one = transactions[i];
-    const result = await axios.post(
-      'http://localhost:4096/peer/transactions',
-      { transaction: one },
-      config
-    );
-    console.log(JSON.stringify(result.data, null, 2));
+    try {
+      const result = await axios.post(
+        'http://localhost:4096/peer/transactions',
+        { transaction: one },
+        config
+      );
+    } catch (err) {
+      console.log(
+        `[trs sending failed] ${err.response ? err.response.data : err.message}`
+      );
+    }
   }
 }
 
@@ -81,36 +118,45 @@ describe('unconfirmed-trs e2e test', () => {
       // node2 and node3 should receive transactions
       await lib.sleep(20 * 1000);
 
+      const trs4098 = new Map<string, UnconfirmedTransaction>();
+      const trs4098Promise = constantQuery(4098, trs4098);
+
+      const trs4100 = new Map<string, UnconfirmedTransaction>();
+      const trs4100Promise = constantQuery(4100, trs4100);
+
       await helpers.allHeightsAreTheSame([4096, 4098, 4100]);
       const height = await lib.getHeight(4096);
       expect(new BigNumber(height).isGreaterThan(2)).toEqual(true);
 
-      let node2Count = 0;
-      let node3Count = 3;
-
       // 1st try
       await sendRandomTransaction(50);
       await lib.sleep(7 * 1000);
-      node2Count += await getUnconfirmedTrsCount(4098);
-      node3Count += await getUnconfirmedTrsCount(4100);
+      console.log(`[1][4098]: "${trs4098.size}"`);
+      console.log(`[1][4100]: "${trs4100.size}"`);
 
       // 2nd try
       await sendRandomTransaction(50);
       await lib.sleep(7 * 1000);
-      node2Count += await getUnconfirmedTrsCount(4098);
-      node3Count += await getUnconfirmedTrsCount(4100);
+      console.log(`[2][4098]: "${trs4098.size}"`);
+      console.log(`[2][4100]: "${trs4100.size}"`);
 
       // 3rd
       await sendRandomTransaction(50);
       await lib.sleep(7 * 1000);
-      node2Count += await getUnconfirmedTrsCount(4098);
-      node3Count += await getUnconfirmedTrsCount(4100);
+      console.log(`[3][4098]: "${trs4098.size}"`);
+      console.log(`[3][4100]: "${trs4100.size}"`);
 
-      // node2 and node3 should have got some unconfirmed Transactions
-      expect(node2Count).toBeGreaterThan(1);
-      expect(node3Count).toBeGreaterThan(1);
+      // stop checkinng unconfirmed transactions
+      continueRequest = false;
+      await trs4098Promise;
 
-      done();
+      expect(trs4098.size).toBeGreaterThan(0);
+      console.log(`final size[4098]: "${trs4098.size}"`);
+
+      expect(trs4100.size).toBeGreaterThan(0);
+      console.log(`final size[4100]: "${trs4100.size}"`);
+
+      return done();
     },
     3 * lib.oneMinute
   );
