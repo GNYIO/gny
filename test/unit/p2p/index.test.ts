0ebfa1e3465e75bfe8b2348f4d5f527dee3bc894
Matthias Knopp <matthias-knopp@gmx.net>
2022-10-19 00:10:09 +0200

    fix: ReferenceError: TextDecoder is not defined (jsdom)

--- test/unit/p2p/index.test.ts
+++ test/unit/p2p/index.test.ts
@@ -1,3 +1,8 @@
+// workourand for: ReferenceError: TextDecoder is not defined
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 import {
   ILogger,
   P2PMessage,
