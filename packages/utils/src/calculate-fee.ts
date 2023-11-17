b8cdb98af2f654dfbc48988a76d01c04efecc98d
Matthias Knopp <matthias-knopp@gmx.net>
2023-11-17 20:46:23 +0100

    chore: remove ml contracts for now
    contracts are unused

--- packages/utils/src/calculate-fee.ts
+++ packages/utils/src/calculate-fee.ts
@@ -3,6 +3,7 @@ export interface FeeCalculatorIndexer {
 }
 
 export const feeCalculators: FeeCalculatorIndexer = {
+  // basic
   0: () => 0.1,
   1: () => 5,
   2: () => 5,
@@ -12,20 +13,12 @@ export const feeCalculators: FeeCalculatorIndexer = {
   6: () => 0,
   10: () => 100,
   20: () => 0.1,
+
+  // uia
   100: () => 100,
   101: () => 500,
   102: () => 0.1,
   103: () => 0.1,
-  201: () => 0.1,
-  202: () => 0.1,
-  203: () => 0.1,
-  204: () => 0.1,
-  205: () => 0.1,
-  206: () => 0.1,
-  207: () => 0.1,
-  208: () => 0.1,
-  209: () => 0.1,
-  210: () => 0.1,
 
   // nft
   300: () => 100,
