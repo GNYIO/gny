63ce34628bf1294f3d4b4be51c84ed20c861dc26
a1300 <matthias-knopp@gmx.net>
2024-04-05 18:55:37 +0000

    fix: revert p2p random fail fix #566

--- packages/p2p/src/wrapper.js
+++ packages/p2p/src/wrapper.js
@@ -80,16 +80,8 @@ export class Bundle extends Libp2p {
 
     const allConnectedPeers = connections.map(x => {
       const peerId = PeerId.createFromB58String(x);
-      const first = this.peerStore.addressBook.get(peerId);
-
-      if (!first) {
-        this.logger.info(
-          `[p2p][getAllConnectedPeersPeerInfo] no peerId for peer: ${x}`
-        );
-        return null;
-      }
-
-      const addresses = first
+      const addresses = this.peerStore.addressBook
+        .get(peerId)
         .map(x => multiaddr(x.multiaddr))
         .map(x => x.encapsulate(`/p2p/${peerId.toB58String()}`));
 
