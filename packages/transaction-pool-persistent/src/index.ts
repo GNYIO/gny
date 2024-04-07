7cd3a08dda291a93b84817af74d388d72d8c2052
a1300 <matthias-knopp@gmx.net>
2024-04-07 18:00:44 +0000

    chore: install @types/better-sqlite3

--- packages/transaction-pool-persistent/src/index.ts
+++ packages/transaction-pool-persistent/src/index.ts
@@ -1,7 +1,44 @@
 import { ITransactionPool, UnconfirmedTransaction } from '@gnyio/interfaces';
+import Sqlite from 'better-sqlite3';
+
+const table1 = `
+
+CREATE TABLE IF NOT EXISTS transactions (
+  id TEXT NOT NULL UNIQUE,
+  type NUMBER NOT NULL,
+  timestamp INT NOT NULL,
+  senderId TEXT NOT NULL,
+  senderPublicKey TEXT NOT NULL,
+  fee TEXT NOT NULL,
+  signatures TEXT NOT NULL,
+  secondSignature TEXT NULL, -- nullable
+  args TEXT NOT NULL,
+  message TEXT NULL,         -- nullable
+  -- _version_ INT NULL,
+  PRIMARY KEY (id)
+) STRICT;
+`;
 
 export class TransactionPoolPersistent implements ITransactionPool {
-  constructor() {}
+  private db!: Sqlite.Database;
+
+  // path: either a path to a sqlite3 file or ":memory:"
+  constructor(path: string) {
+    this.db = new Sqlite(path, {
+      readonly: false,
+      fileMustExist: false,
+      timeout: 5000,
+      // verbose: (x) => console.log(`[SQL] ${x}`),
+    });
+
+    this.db.pragma('journal_mode = WAL');
+
+    // configure bigint
+    // https://github.com/WiseLibs/better-sqlite3/blob/master/docs/integer.md#the-bigint-primitive-type
+    this.db.defaultSafeIntegers(false); // Numbers by default
+
+    this.db.exec(table1);
+  }
 
   public add(trs: UnconfirmedTransaction) {}
 
