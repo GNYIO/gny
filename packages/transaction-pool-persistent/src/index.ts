e22925907e6745bca5f5452f64dfc2c9f3d259b4
a1300 <matthias-knopp@gmx.net>
2024-04-08 08:45:22 +0000

    chore: add implementation of @gnyio/transaction-pool-persistent

--- packages/transaction-pool-persistent/src/index.ts
+++ packages/transaction-pool-persistent/src/index.ts
@@ -5,7 +5,7 @@ const table1 = `
 
 CREATE TABLE IF NOT EXISTS transactions (
   id TEXT NOT NULL UNIQUE,
-  type NUMBER NOT NULL,
+  type INT NOT NULL,
   timestamp INT NOT NULL,
   senderId TEXT NOT NULL,
   senderPublicKey TEXT NOT NULL,
@@ -18,6 +18,22 @@ CREATE TABLE IF NOT EXISTS transactions (
   PRIMARY KEY (id)
 ) STRICT;
 `;
+
+function cleanUp(result: any) {
+  if (typeof result.args === 'string') {
+    result.args = JSON.parse(result.args);
+  }
+
+  if (result.message === null) {
+    result.message = undefined;
+  }
+
+  if (result.secondSignature === null) {
+    delete result.secondSignature;
+  }
+
+  return result;
+}
 
 export class TransactionPoolPersistent implements ITransactionPool {
   private db!: Sqlite.Database;
@@ -40,15 +56,112 @@ export class TransactionPoolPersistent implements ITransactionPool {
     this.db.exec(table1);
   }
 
-  public add(trs: UnconfirmedTransaction) {}
+  public add(trs: UnconfirmedTransaction) {
+    try {
+      this.db
+        .prepare(
+          `
+        INSERT INTO transactions (id, type, timestamp, senderId, senderPublicKey, fee, signatures, secondSignature, args, message)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+      `
+        )
+        .run(
+          trs.id,
+          trs.type,
+          trs.timestamp,
+          trs.senderId,
+          trs.senderPublicKey,
+          trs.fee,
+          trs.signatures,
+          trs.secondSignature,
+          JSON.stringify(trs.args),
+          trs.message
+        );
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-  public remove(id: string) {}
+  public remove(id: string) {
+    const result = this.db
+      .prepare(
+        `
+      DELETE FROM transactions
+      WHERE id = ?;
+    `
+      )
+      .run(id);
 
-  public has(id: string) {}
+    // if (result.changes !== 1) {
+    //   throw new Error('SQLITE not exactly 1 row updated');
+    // }
+  }
 
-  public getUnconfirmed() {}
+  public has(id: string) {
+    interface ZeroOrOne {
+      result: 0 | 1;
+    }
 
-  public clear() {}
+    // https://stackoverflow.com/questions/9755860/valid-query-to-check-if-row-exists-in-sqlite3
+    const raw = this.db
+      .prepare(
+        `
+      SELECT EXISTS(
+        SELECT 1 FROM transactions
+        WHERE id = ?
+      ) as result;
+    `
+      )
+      .get(id) as ZeroOrOne;
 
-  public get(id: string) {}
+    if (raw.result === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public getUnconfirmed() {
+    const temp = this.db
+      .prepare(
+        `
+      SELECT *
+      FROM transactions;
+    `
+      )
+      .all() as UnconfirmedTransaction[];
+
+    const result = temp.map(x => cleanUp(x));
+    return result;
+  }
+
+  public clear() {
+    this.db
+      .prepare(
+        `
+      DELETE FROM transactions;
+    `
+      )
+      .run();
+  }
+
+  public get(id: string) {
+    const temp = this.db
+      .prepare(
+        `
+      SELECT *
+      FROM transactions
+      WHERE id = ?;
+    `
+      )
+      .get(id) as UnconfirmedTransaction;
+
+    if (temp === undefined) {
+      return undefined;
+    }
+
+    const result = cleanUp(temp);
+
+    return result;
+  }
 }
