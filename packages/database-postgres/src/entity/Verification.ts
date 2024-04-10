0be19c347bf7eca25669eb3753a2ccf0c397fe49
a1300 <matthias-knopp@gmx.net>
2024-04-10 12:59:12 +0000

    chore: add "verification" table [ci skip]

---
+++ packages/database-postgres/src/entity/Verification.ts
@@ -0,0 +1,51 @@
+import { Column, Entity, PrimaryColumn, Index } from 'typeorm';
+import { Config } from '../decorator/config.js';
+import { Versioned } from '../searchTypes.js';
+
+@Config({ memory: false })
+@Entity()
+export class Verification implements Versioned {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: 128,
+    nullable: false,
+  })
+  public identifier: string;
+
+  @Column({
+    length: 128,
+    type: 'varchar',
+    nullable: false,
+  })
+  @Index()
+  public tid: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
+  @Index()
+  public senderId: string;
+
+  @Column({
+    length: 128,
+    nullable: false,
+    type: 'varchar',
+  })
+  public signature: string;
+
+  @Column({
+    nullable: false,
+    type: 'int',
+  })
+  @Index()
+  public timestamp: number;
+
+  @Column({
+    default: 0,
+    type: 'integer',
+    nullable: false,
+  })
+  public _version_?: number;
+}
