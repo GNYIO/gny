758f769f4054e6e87c57b89bc36c70b5291aab62
a1300 <matthias-knopp@gmx.net>
2024-04-12 09:40:57 +0000

    chore: verify signature (WIP) [ci skip]

--- packages/web-base/src/messageBase.ts
+++ packages/web-base/src/messageBase.ts
@@ -4,12 +4,13 @@ import * as helpers from './helpers';
 
 // to sign and verify custom verifications
 export class MessageWebBase {
+  // signs with private key
   public static sign(message: string, keypair: KeyPair) {
-    const hash = helpers.sha256Bytes(message);
-    // return hash;
-
-    return webEd.sign(hash, keypair.privateKey).toString('hex');
+    return webEd.sign(message, keypair.privateKey).toString('hex');
   }
 
-  public static verify() {}
+  public static verify(message: string, signature: string, publicKey: string) {
+    const c: boolean = webEd.verify(message, signature, publicKey);
+    return result;
+  }
 }
